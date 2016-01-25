e53b0e7351f6122b058644379d8cf42354303c7b
Thien Nguyen <Thien Nguyen>
2016-01-25 07:18:50 +0700

    add product list

---
+++ src/HvCommerce.Web/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using HvCommerce.Core.Domain.Models;
+using HvCommerce.Infrastructure.Domain.IRepositories;
+using HvCommerce.Web.Areas.Admin.ViewModels;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Mvc;
+
+namespace HvCommerce.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "admin")]
+    public class ProductController : Controller
+    {
+        private readonly IRepository<Product> productRepository;
+
+        public ProductController(IRepository<Product> productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult ListAjax([DataSourceRequest] DataSourceRequest request)
+        {
+            var products = productRepository.Query().Where(x => !x.IsDeleted);
+            var gridData = products.ToDataSourceResult(
+                request,
+                x => new ProductListItem
+                    {
+                        Id = x.Id,
+                        Name = x.Name,
+                        CreatedOn = x.CreatedOn,
+                        IsPublished = x.IsPublished
+                    });
+
+            return Json(gridData);
+        }
+    }
+}
---
+++ src/HvCommerce.Web/Areas/Admin/ViewModels/ProductListItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HvCommerce.Web.Areas.Admin.ViewModels
+{
+    public class ProductListItem
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public bool IsPublished { get; set; }
+    }
+}
