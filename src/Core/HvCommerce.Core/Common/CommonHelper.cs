589ad9a1e4e92297884200974a5ddc16dff3d072
CangPham <cang.phamsy@harveynash.vn>
2016-02-22 15:42:41 +0700

    add common helper
    add some function common such as IsValidEmail, NumericOnly, NotNull,
    AreNullOrEmpty, ArraysEqual ...

---
+++ src/Core/HvCommerce.Core/Common/CommonHelper.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace HvCommerce.Core.Common
+{
+    public class CommonHelper
+    {
+        /// <summary>
+        /// Verifies that a string is in valid e-mail format
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        public static bool IsValidEmail(string email)
+        {
+            if (String.IsNullOrEmpty(email))
+                return false;
+
+            email = email.Trim();
+            var result = Regex.IsMatch(email, "^(?:[\\w\\!\\#\\$\\%\\&\\'\\*\\+\\-\\/\\=\\?\\^\\`\\{\\|\\}\\~]+\\.)*[\\w\\!\\#\\$\\%\\&\\'\\*\\+\\-\\/\\=\\?\\^\\`\\{\\|\\}\\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\\-](?!\\.)){0,61}[a-zA-Z0-9]?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\\[(?:(?:[01]?\\d{1,2}|2[0-4]\\d|25[0-5])\\.){3}(?:[01]?\\d{1,2}|2[0-4]\\d|25[0-5])\\]))$", RegexOptions.IgnoreCase);
+            return result;
+        }
+
+        /// <summary>
+        /// string input only contains numeric values
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string NumericOnly(string str)
+        {
+            if (String.IsNullOrEmpty(str))
+                return string.Empty;
+
+            var result = new StringBuilder();
+            foreach (char c in str)
+            {
+                if (Char.IsDigit(c))
+                    result.Append(c);
+            }
+            return result.ToString();
+        }
+
+        /// <summary>
+        /// ensure that string never null
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string NotNull(string str)
+        {
+            if (str == null)
+                return string.Empty;
+
+            return str;
+        }
+
+        public static bool AreNullOrEmpty(params string[] stringsToValidate)
+        {
+            bool result = false;
+            Array.ForEach(stringsToValidate, str =>
+            {
+                if (string.IsNullOrEmpty(str)) result = true;
+            });
+            return result;
+        }
+
+        public static bool ArraysEqual<T>(T[] a1, T[] a2)
+        {
+            if (ReferenceEquals(a1, a2))
+                return true;
+
+            if (a1 == null || a2 == null)
+                return false;
+
+            if (a1.Length != a2.Length)
+                return false;
+
+            var comparer = EqualityComparer<T>.Default;
+            for (int i = 0; i < a1.Length; i++)
+            {
+                if (!comparer.Equals(a1[i], a2[i])) return false;
+            }
+            return true;
+        }
+
+        public static string ConvertEnum(string str)
+        {
+            string result = string.Empty;
+            char[] letters = str.ToCharArray();
+            foreach (char c in letters)
+                if (c.ToString() != c.ToString().ToLower())
+                    result += " " + c.ToString();
+                else
+                    result += c.ToString();
+            return result;
+        }
+
+        public static string GenerateRandomDigitCode(int length)
+        {
+            var random = new Random();
+            string str = string.Empty;
+            for (int i = 0; i < length; i++)
+                str = String.Concat(str, random.Next(10).ToString());
+            return str;
+        }
+
+        public static int GenerateRandomInteger(int min = 0, int max = int.MaxValue)
+        {
+            var randomNumberBuffer = new byte[10];
+            new RNGCryptoServiceProvider().GetBytes(randomNumberBuffer);
+            return new Random(BitConverter.ToInt32(randomNumberBuffer, 0)).Next(min, max);
+        }
+    }
+}
