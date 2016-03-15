dea97c8cceb9a6d9d451b2aa7b761c29fecfd00b
Thien Nguyen <nlqthien@gmail.com>
2016-03-15 16:43:32 +0700

    replace flexslider by smoothproducts

--- src/HvCommerce.Web/wwwroot/js/product-detail.js
+++ src/HvCommerce.Web/wwwroot/js/product-detail.js
@@ -1,22 +1,5 @@
-﻿(function() {
+﻿(function () {
     $(window).load(function () {
-        // The slider being synced must be initialized first
-        $('#carousel').flexslider({
-            animation: "slide",
-            controlNav: false,
-            animationLoop: false,
-            slideshow: false,
-            itemWidth: 210,
-            itemMargin: 5,
-            asNavFor: '#slider'
-        });
-
-        $('#slider').flexslider({
-            animation: "slide",
-            controlNav: false,
-            animationLoop: false,
-            slideshow: false,
-            sync: "#carousel"
-        });
+        $('.sp-wrap').smoothproducts();
     });
-})()
+})();
