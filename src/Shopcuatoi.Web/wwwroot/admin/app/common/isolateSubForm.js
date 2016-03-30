ded0f962ac169e6cf421090253018cd2128aa76d
Thien Nguyen <nlqthien@gmail.com>
2016-03-30 14:37:34 +0700

    add isolated sub form

---
+++ src/Shopcuatoi.Web/wwwroot/admin/app/common/isolateSubForm.js
@@ -0,0 +1,37 @@
+﻿(function () {
+    angular.module('shopAdmin.common').
+    directive('isolateForm', [function () {
+        return {
+            restrict: 'A',
+            require: '?form',
+            link: function (scope, elm, attrs, ctrl) {
+                if (!ctrl) {
+                    return;
+                }
+
+                // Do a copy of the controller
+                var ctrlCopy = {};
+                angular.copy(ctrl, ctrlCopy);
+
+                // Get the parent of the form
+                var parent = elm.parent().controller('form');
+                // Remove parent link to the controller
+                parent.$removeControl(ctrl);
+
+                // Replace form controller with a "isolated form"
+                var isolatedFormCtrl = {
+                    $setValidity: function (validationToken, isValid, control) {
+                        ctrlCopy.$setValidity(validationToken, isValid, control);
+                        parent.$setValidity(validationToken, true, ctrl);
+                    },
+                    $setDirty: function () {
+                        elm.removeClass('ng-pristine').addClass('ng-dirty');
+                        ctrl.$dirty = true;
+                        ctrl.$pristine = false;
+                    },
+                };
+                angular.extend(ctrl, isolatedFormCtrl);
+            }
+        };
+    }]);
+})();
