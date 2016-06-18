76eb84b55c314ba0efc5df1fbdbfc8705683c9f7
thiennn <nlqthien@gmail.com>
2016-06-18 23:42:13 +0700

    small ui enhancement

---
+++ src/SimplCommerce.Web/wwwroot/js/main.js
@@ -0,0 +1,19 @@
+﻿/*global jQuery, window*/
+(function ($) {
+    $(window).load(function () {
+        $('.lang-selector li').on('click', function (e) {
+            var lang = $(this).find('a').attr('data-value'),
+                $langForm = $('#lang-form'),
+                $cultureInput = $langForm.find('input[name=culture]');
+
+            if ($cultureInput.val() === lang) {
+                e.preventDefault();
+                return;
+            }
+            else {
+                $cultureInput.val(lang);
+                $langForm.submit();
+            }
+        });
+    });
+})(jQuery);
