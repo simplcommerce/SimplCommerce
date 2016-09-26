de730f53ac8aa968207c2c47f2fec29751911b90
Thien Nguyen <nlqthien@gmail.com>
2016-09-26 09:21:43 +0700

    #63 set Product_RatingEverage to nullable

--- src/SimplCommerce.WebHost/Migrations/20160926021822_SetProduct_RatingAverageNullable.Designer.cs
+++ src/SimplCommerce.WebHost/Migrations/20160926021822_SetProduct_RatingAverageNullable.Designer.cs
@@ -8,9 +8,10 @@ using SimplCommerce.Module.Core.Data;
 namespace SimplCommerce.WebHost.Migrations
 {
     [DbContext(typeof(SimplDbContext))]
-    partial class SimplDbContextModelSnapshot : ModelSnapshot
+    [Migration("20160926021822_SetProduct_RatingAverageNullable")]
+    partial class SetProduct_RatingAverageNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1")
---
+++ src/SimplCommerce.WebHost/Migrations/20160926021822_SetProduct_RatingAverageNullable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SimplCommerce.WebHost.Migrations
+{
+    public partial class SetProduct_RatingAverageNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "RatingAverage",
+                table: "Catalog_Product",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "RatingAverage",
+                table: "Catalog_Product",
+                nullable: false);
+        }
+    }
+}
