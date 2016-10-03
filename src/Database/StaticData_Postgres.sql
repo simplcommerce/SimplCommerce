800a5b47c72dfd1128a5aa489c6382f1a233fca4
thiennn <nlqthien@gmail.com>
2016-10-03 23:43:26 +0700

    #66 fix syntax error in StaticData_Postgres.sql

--- src/Database/StaticData_Postgres.sql
+++ src/Database/StaticData_Postgres.sql
@@ -9,13 +9,13 @@ SELECT pg_catalog.setval('"Core_User_Id_seq"', 1, true);
 
 INSERT INTO "Core_UserRole" ("UserId", "RoleId") VALUES (1, 1);
 
-INSERT INTO "Core_EntityType" ("Id", "Name", "RoutingController", "RoutingAction") VALUES (1, 'Category', 'Category', 'CategoryDetail')
-INSERT INTO "Core_EntityType" ("Id", "Name", "RoutingController", "RoutingAction") VALUES (2, 'Brand', 'Brand', 'BrandDetail')
-INSERT INTO "Core_EntityType" ("Id", "Name", "RoutingController", "RoutingAction") VALUES (3, 'Product', 'Product', 'ProductDetail')
-INSERT INTO "Core_EntityType" ("Id", "Name", "RoutingController", "RoutingAction") VALUES (4, 'Page', 'Page', 'PageDetail')
+INSERT INTO "Core_EntityType" ("Id", "Name", "RoutingController", "RoutingAction") VALUES (1, 'Category', 'Category', 'CategoryDetail');
+INSERT INTO "Core_EntityType" ("Id", "Name", "RoutingController", "RoutingAction") VALUES (2, 'Brand', 'Brand', 'BrandDetail');
+INSERT INTO "Core_EntityType" ("Id", "Name", "RoutingController", "RoutingAction") VALUES (3, 'Product', 'Product', 'ProductDetail');
+INSERT INTO "Core_EntityType" ("Id", "Name", "RoutingController", "RoutingAction") VALUES (4, 'Page', 'Page', 'PageDetail');
 SELECT pg_catalog.setval('"Core_EntityType_Id_seq"', 4, true);
 
-INSERT INTO "ActivityLog_ActivityType" ("Id", "Name") VALUES (1, 'ProductView')
+INSERT INTO "ActivityLog_ActivityType" ("Id", "Name") VALUES (1, 'ProductView');
 SELECT pg_catalog.setval('"ActivityLog_ActivityType_Id_seq"', 1, true);
 
 INSERT INTO "Core_Widget" ("Id", "Code", "CreateUrl", "CreatedOn", "EditUrl", "IsPublished", "Name", "ViewComponentName") VALUES (1, 'CarouselWidget', 'widget-carousel-create', '2016-06-19 00:00:00', 'widget-carousel-edit', true, 'Carousel Widget', 'CarouselWidget');
