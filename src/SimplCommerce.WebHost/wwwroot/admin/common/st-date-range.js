6e3fc14a45ab74455c82f09d0a6e32ecf9b985e5
thiennn <nlqthien@gmail.com>
2016-11-11 21:51:50 +0700

    search on datepicker changed instead of blur on the textbox

--- src/SimplCommerce.WebHost/wwwroot/admin/common/st-date-range.js
+++ src/SimplCommerce.WebHost/wwwroot/admin/common/st-date-range.js
@@ -14,29 +14,20 @@
                 },
                 templateUrl: 'admin/common/st-date-range.html',
                 link: function (scope, element, attr, table) {
-                    var inputs = element.find('input'),
-                        inputBefore = angular.element(inputs[0]),
-                        inputAfter = angular.element(inputs[1]),
-                        predicateName = attr.predicate;
+                    var predicateName = attr.predicate;
 
-                    [inputBefore, inputAfter].forEach(function (input) {
-                        input.bind('blur', function () {
-                            var query = {};
-                            if (!scope.isBeforeOpen && !scope.isAfterOpen) {
-                                if (scope.before) {
-                                    query.before = scope.before;
-                                }
+                    scope.changeDate = function() {
+                        var query = {};
+                        if (scope.before) {
+                            query.before = scope.before;
+                        }
 
-                                if (scope.after) {
-                                    query.after = scope.after;
-                                }
+                        if (scope.after) {
+                            query.after = scope.after;
+                        }
 
-                                scope.$apply(function () {
-                                    table.search(query, predicateName);
-                                });
-                            }
-                        });
-                    });
+                        table.search(query, predicateName);
+                    };
 
                     function open(before) {
                         return function ($event) {
