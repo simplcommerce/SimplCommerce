73ab0799fbf99f7aca220ada224c6b9e37a5b5e7
thiennn <nlqthien@gmail.com>
2016-11-12 21:56:54 +0700

    localize string on postgresql add gulp task to copy modue static content only

--- src/SimplCommerce.WebHost/gulpfile.js
+++ src/SimplCommerce.WebHost/gulpfile.js
@@ -26,13 +26,22 @@ gulp.task('clean-module', function () {
     .pipe(clean());
 });
 
-
 gulp.task('copy-modules', ['clean-module'], function () {
     modules.forEach(function (module) {
         gulp.src([paths.devModules + module + '/Views/**/*.*', paths.devModules + module + '/wwwroot/**/*.*'], { base: module })
             .pipe(gulp.dest(paths.hostModules + module));
         gulp.src(paths.devModules + module + '/bin/Debug/netstandard1.6/**/*.*')
             .pipe(gulp.dest(paths.hostModules + module + '/bin'));
+    });
+
+    gulp.src(paths.devModules + 'SimplCommerce.Module.SampleData/SampleContent/**/*.*')
+            .pipe(gulp.dest(paths.hostModules + 'SimplCommerce.Module.SampleData/SampleContent'));
+});
+
+gulp.task('copy-ms', function () {
+    modules.forEach(function (module) {
+        gulp.src([paths.devModules + module + '/Views/**/*.*', paths.devModules + module + '/wwwroot/**/*.*'], { base: module })
+            .pipe(gulp.dest(paths.hostModules + module));
     });
 
     gulp.src(paths.devModules + 'SimplCommerce.Module.SampleData/SampleContent/**/*.*')
