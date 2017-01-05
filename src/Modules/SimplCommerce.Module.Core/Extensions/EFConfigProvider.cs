3e482fa6eff950a87478249e08a932f2c57ff3f8
Thien Nguyen Le Quynh <nlqthien@gmail.com>
2017-01-05 13:16:03 +0700

    Configuration management (#100)
    #98 configuration management

---
+++ src/Modules/SimplCommerce.Module.Core/Extensions/EFConfigProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace SimplCommerce.Module.Core.Extensions
+{
+    public class EFConfigProvider : ConfigurationProvider
+    {
+        public EFConfigProvider(Action<DbContextOptionsBuilder> optionsAction)
+        {
+            OptionsAction = optionsAction;
+        }
+
+        Action<DbContextOptionsBuilder> OptionsAction { get; }
+
+        // Load config data from EF DB.
+        public override void Load()
+        {
+            var builder = new DbContextOptionsBuilder<EFConfigurationDbContext>();
+            OptionsAction(builder);
+
+            using (var dbContext = new EFConfigurationDbContext(builder.Options))
+            {
+                try
+                {
+                    Data = dbContext.AppSettings.ToDictionary(c => c.Key, c => c.Value);
+                }
+                catch (Exception ex)
+                {
+                    // TODO:This might fail when run EF CLI because the table was not there. Need some workaround
+                }
+            }
+        }
+    }
+}
--- src/SimplCommerce.WebHost/Migrations/20170104231817_AddAppSetting.Designer.cs
+++ src/SimplCommerce.WebHost/Migrations/20170104231817_AddAppSetting.Designer.cs
@@ -8,9 +8,10 @@ using SimplCommerce.Module.Core.Data;
 namespace SimplCommerce.WebHost.Migrations
 {
     [DbContext(typeof(SimplDbContext))]
-    partial class SimplDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170104231817_AddAppSetting")]
+    partial class AddAppSetting
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.0-rtm-22752")
