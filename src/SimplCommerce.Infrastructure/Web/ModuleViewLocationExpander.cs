7efa8c46d4dad34f8682f4acb9922e1ba772a510
Thien Nguyen <nlqthien@gmail.com>
2017-01-18 18:00:52 +0700

    #13 theming support

--- src/SimplCommerce.Infrastructure/Web/ModuleViewLocationExpander.cs
+++ src/SimplCommerce.Infrastructure/Web/ModuleViewLocationExpander.cs
@@ -1,29 +1,52 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace SimplCommerce.Infrastructure.Web
 {
     public class ModuleViewLocationExpander : IViewLocationExpander
     {
-        private const string _moduleKey = "module";
+        private const string MODULE_KEY = "module";
+        private const string THEME_KEY = "theme";
 
         public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
         {
-            if (context.Values.ContainsKey(_moduleKey))
+            if (context.Values.ContainsKey(THEME_KEY) && !string.IsNullOrWhiteSpace(context.Values[THEME_KEY]))
             {
-                var module = context.Values[_moduleKey];
-                if (!string.IsNullOrWhiteSpace(module))
+                var theme = context.Values[THEME_KEY];
+
+                if (context.Values.ContainsKey(MODULE_KEY) && !string.IsNullOrWhiteSpace(context.Values[MODULE_KEY]))
                 {
+                    var module = context.Values[MODULE_KEY];
                     var moduleViewLocations = new string[]
                     {
-                    "/Modules/SimplCommerce.Module." + module + "/Views/{1}/{0}.cshtml",
-                    "/Modules/SimplCommerce.Module." + module + "/Views/Shared/{0}.cshtml"
+                        $"/Themes/{theme}/Modules/SimplCommerce.Module.{module}/Views/{{1}}/{{0}}.cshtml",
+                        $"/Themes/{theme}/Modules/SimplCommerce.Module.{module}/Views/Shared/{{0}}.cshtml",
+                        $"/Modules/SimplCommerce.Module.{module}/Views/{{1}}/{{0}}.cshtml",
+                        $"/Modules/SimplCommerce.Module.{module}/Views/Shared/{{0}}.cshtml",
+                        $"/Themes/{theme}/Views/Shared/{{0}}.cshtml"
                     };
 
                     viewLocations = moduleViewLocations.Concat(viewLocations);
                 }
             }
+            else
+            {
+                if (context.Values.ContainsKey(MODULE_KEY) && !string.IsNullOrWhiteSpace(context.Values[MODULE_KEY]))
+                {
+                    var module = context.Values[MODULE_KEY];
+                    var moduleViewLocations = new string[]
+                    {
+                        $"/Modules/SimplCommerce.Module.{module}/Views/{{1}}/{{0}}.cshtml",
+                        $"/Modules/SimplCommerce.Module.{module}/Views/Shared/{{0}}.cshtml"
+                    };
+
+                    viewLocations = moduleViewLocations.Concat(viewLocations);
+                }
+            }
+
             return viewLocations;
         }
 
@@ -31,7 +54,10 @@ namespace SimplCommerce.Infrastructure.Web
         {
             var controller = context.ActionContext.ActionDescriptor.DisplayName;
             var moduleName = controller.Split('.')[2];
-            context.Values[_moduleKey] = moduleName;
+            context.Values[MODULE_KEY] = moduleName;
+
+            var config = context.ActionContext.HttpContext.RequestServices.GetService<IConfiguration>();
+            context.Values[THEME_KEY] = config["theme"];
         }
     }
 }
