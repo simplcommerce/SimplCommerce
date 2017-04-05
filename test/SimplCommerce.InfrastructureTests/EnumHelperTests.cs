a0c2c101750dd7626827710d96122fd6c462cff8
Maher Jendoubi <maher.jendoubi@gmail.com>
2017-04-05 23:54:05 +0200

    Added EnumHelperTests

---
+++ test/SimplCommerce.InfrastructureTests/EnumHelperTests.cs
@@ -0,0 +1,24 @@
+﻿using SimplCommerce.Infrastructure;
+using Xunit;
+
+namespace SimplCommerce.InfrastructureTests
+{
+    public class EnumHelperTests
+    {
+        enum Importance
+        {
+            None,
+            Trivial,
+            Regular,
+            Important,
+            Critical
+        };
+
+        [Fact]
+        public void ImportanceEnumCriticalShouldReturnsCritical()
+        {
+            var foo = Importance.Critical.GetDisplayName();
+            Assert.Equal("Critical", foo);
+        }
+    }
+}
