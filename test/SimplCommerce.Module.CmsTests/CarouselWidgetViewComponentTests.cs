2bc5093720f8e0c3690b9e9ce2e52d4741ff8955
Maher Jendoubi <maher.jendoubi@gmail.com>
2017-04-05 12:00:42 +0200

    Added CmsTests

---
+++ test/SimplCommerce.Module.CmsTests/CarouselWidgetViewComponentTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimplCommerce.Module.CmsTests
+{
+    public class CarouselWidgetViewComponentTests
+    {
+    }
+}
