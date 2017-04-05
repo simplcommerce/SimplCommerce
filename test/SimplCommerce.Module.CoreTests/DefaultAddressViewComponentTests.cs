8c22202b7f0ef35d47782e4885cb3e0d9330bedc
Maher Jendoubi <maher.jendoubi@gmail.com>
2017-04-05 11:47:13 +0200

    Rename of unit test project

---
+++ test/SimplCommerce.Module.CoreTests/DefaultAddressViewComponentTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace SimplCommerce.Module.CoreTests
+{
+    public class DefaultAddressViewComponentTests
+    {
+        [Fact]
+        public Task<IViewComponentResult> InvokeAsyncTest()
+        {
+            return null;
+        }
+    }
+}
