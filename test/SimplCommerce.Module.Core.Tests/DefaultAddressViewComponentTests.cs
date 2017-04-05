0e39adaecef880e8c55cdb0d8174011284b54777
Maher Jendoubi <maher.jendoubi@gmail.com>
2017-04-05 11:40:39 +0200

    added unit tests project for Core Module

---
+++ test/SimplCommerce.Module.Core.Tests/DefaultAddressViewComponentTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace SimplCommerce.Module.Core.Tests
+{
+    public class DefaultAddressViewComponentTests
+    {
+        [Fact]
+        public Task<IViewComponentResult> InvokeAsyncTest()
+        {
+            return null;
+        }
+    }
+}
