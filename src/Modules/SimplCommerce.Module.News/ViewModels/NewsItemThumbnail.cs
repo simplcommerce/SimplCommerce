76eb26beabbfd8b3e44563455711cdedcfba0c78
Khuong Phan <duykhuongkid@gmail.com>
2017-04-19 08:14:48 +0700

    Add front end for News Category

---
+++ src/Modules/SimplCommerce.Module.News/ViewModels/NewsItemThumbnail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimplCommerce.Module.News.ViewModels
+{
+    public class NewsItemThumbnail
+    {
+        public long Id { get; set; }
+
+        public string ShortContent { get; set; }
+        
+        public string ImageUrl { get; set; }
+
+        public DateTimeOffset PublishedOn { get; set; }
+    }
+}
