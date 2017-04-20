55cf191bfa6a2c241c09e0e884771586cb88666e
Thien Nguyen <nlqthien@gmail.com>
2017-04-20 07:33:26 +0700

    added news landing page

--- src/Modules/SimplCommerce.Module.News/Controllers/NewsItemController.cs
+++ src/Modules/SimplCommerce.Module.News/Controllers/NewsItemController.cs
@@ -12,14 +12,68 @@ namespace SimplCommerce.Module.News.Controllers
     public class NewsItemController : Controller
     {
         private readonly IRepository<NewsItem> _newsItemRepository;
+        private readonly IRepository<NewsCategory> _newsCategoryRepository;
         private readonly IMediaService _mediaService;
+        private int _pageSize;
 
         public NewsItemController(IRepository<NewsItem> newsItemRepository,
+             IRepository<NewsCategory> newsCategoryRepository,
             IMediaService mediaService,
             IConfiguration config)
         {
             _newsItemRepository = newsItemRepository;
+            _newsCategoryRepository = newsCategoryRepository;
             _mediaService = mediaService;
+            _pageSize = config.GetValue<int>("News.PageSize");
+        }
+
+        [HttpGet("news")]
+        public IActionResult NewsHome(int page)
+        {
+            var newsCategoryList = _newsCategoryRepository.Query()
+                .Include(x => x.NewsItems)
+                .Where(x => !x.IsDeleted)
+                .Select(x => new NewsCategoryVm()
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    SeoTitle = x.SeoTitle
+                })
+                .ToList();
+
+            var model = new NewsVm()
+            {
+                NewsCategory = newsCategoryList
+            };
+
+            var query = _newsItemRepository.Query()
+                .Where(x => !x.IsDeleted && x.IsPublished)
+                .OrderByDescending(x => x.CreatedOn);
+
+            model.TotalItem = query.Count();
+            var currentPageNum = page <= 0 ? 1 : page;
+            var offset = (_pageSize * currentPageNum) - _pageSize;
+            while (currentPageNum > 1 && offset >= model.TotalItem)
+            {
+                currentPageNum--;
+                offset = (_pageSize * currentPageNum) - _pageSize;
+            }
+
+            model.NewsItem = query.Include(x => x.ThumbnailImage).Select(x => new NewsItemThumbnail()
+            {
+                Id = x.Id,
+                ShortContent = x.ShortContent,
+                ImageUrl = _mediaService.GetMediaUrl(x.ThumbnailImage),
+                PublishedOn = x.CreatedOn,
+                SeoTitle = x.SeoTitle
+            })
+            .Skip(offset)
+            .Take(_pageSize)
+            .ToList();
+
+            model.PageSize = _pageSize;
+            model.Page = currentPageNum;
+            return View(model);
         }
 
         public IActionResult NewsItemDetail(long id)
