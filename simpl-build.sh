6533c69a1343df93b6fd45e9647f6c0ca2f1ccf7
Thien Nguyen <nlqthien@gmail.com>
2017-06-02 10:57:28 +0700

    correct the ef core version in mac and kestrel port in docker

--- simpl-build.sh
+++ simpl-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-sed -i'' -e 's|<PackageReference Include="Microsoft.EntityFrameworkCore.SqlServer" Version="1.1.0" />|<PackageReference Include="Npgsql.EntityFrameworkCore.PostgreSQL" Version="1.1.0" />|' src/SimplCommerce.WebHost/SimplCommerce.WebHost.csproj
+sed -i'' -e 's|<PackageReference Include="Microsoft.EntityFrameworkCore.SqlServer" Version="2.0.0-preview1-final" />|<PackageReference Include="Npgsql.EntityFrameworkCore.PostgreSQL" Version="2.0.0-preview1" />|' src/SimplCommerce.WebHost/SimplCommerce.WebHost.csproj
 sed -i'' -e 's/UseSqlServer/UseNpgsql/' src/SimplCommerce.WebHost/Startup.cs
 sed -i'' -e 's/UseSqlServer/UseNpgsql/' src/SimplCommerce.WebHost/Extensions/ServiceCollectionExtensions.cs
 
