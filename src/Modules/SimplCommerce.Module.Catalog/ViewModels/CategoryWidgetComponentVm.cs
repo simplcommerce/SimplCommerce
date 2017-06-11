4ee92751ed6b4d133e9c1d32fad732165abd7358
arst <artem.startsev@gmail.com>
2017-06-11 18:22:39 +0300

    Category widget

---
+++ src/Modules/SimplCommerce.Module.Catalog/ViewModels/CategoryWidgetComponentVm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimplCommerce.Module.Catalog.ViewModels
+{
+    public class CategoryWidgetComponentVm
+    {
+        public long Id { get; set; }
+
+        public string WidgetName { get; set; }
+
+        public CategoryThumbnail Category { get; set; }
+    }
+}
