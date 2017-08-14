5e65b0d5f1e338a854b405e77a2de03f36141546
Thien Nguyen <nlqthien@gmail.com>
2017-08-15 03:48:17 +0700

    upgraded to aspnetcore 2.0 rtm

--- src/Modules/SimplCommerce.Module.Core/Models/Role.cs
+++ src/Modules/SimplCommerce.Module.Core/Models/Role.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace SimplCommerce.Module.Core.Models
 {
-    public class Role : IdentityRole<long, UserRole, IdentityRoleClaim<long>>, IEntityWithTypedId<long>
+    public class Role : IdentityRole<long>, IEntityWithTypedId<long>
     {
         public IList<UserRole> Users { get; set; } = new List<UserRole>();
     }
