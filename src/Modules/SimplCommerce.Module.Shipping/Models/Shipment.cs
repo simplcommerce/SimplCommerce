8b6b8642293a15e18e552966a7db66d48bc6c91a
Thien Nguyen <nlqthien@gmail.com>
2017-11-06 04:19:02 +0700

    #214 adding shipping

---
+++ src/Modules/SimplCommerce.Module.Shipping/Models/Shipment.cs
@@ -0,0 +1,24 @@
+﻿using SimplCommerce.Infrastructure.Models;
+using System;
+
+namespace SimplCommerce.Module.Shipping.Models
+{
+    public class Shipment : EntityBase
+    {
+        protected Shipment()
+        {
+            CreatedOn = DateTimeOffset.Now;
+            UpdatedOn = DateTimeOffset.Now;
+        }
+
+        public long OrderId { get; set; }
+
+        public long UserId { get; set; }
+
+        public string TrackingNumber { get; set; }
+
+        public DateTimeOffset CreatedOn { get; set; }
+
+        public DateTimeOffset UpdatedOn { get; set; }
+    }
+}
