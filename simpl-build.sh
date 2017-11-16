6f87e9497ac584f8d08d58c3a8b90646035261c4
IevgenZub <euginzub@mail.ru>
2017-11-16 18:14:45 +0200

    Update simpl-build.sh

--- simpl-build.sh
+++ simpl-build.sh
@@ -12,11 +12,4 @@ dotnet restore && dotnet build
 cd src/SimplCommerce.WebHost \
 	&& npm install \
 	&& npm install --global gulp-cli \
-	&& gulp copy-modules \
-	&& dotnet ef migrations add initialSchema \
-	&& sed -i'' -e '/using SimplCommerce.Module.*.Models;/d' Migrations/SimplDbContextModelSnapshot.cs \
-	&& sed -i'' -e '/using SimplCommerce.Module.*.Models;/d' Migrations/*_initialSchema.Designer.cs \
-	&& dotnet ef database update
-	
-echo "The database schema has been created. Please execute the src/Database/StaticData_Postgres.sql to insert static data."
-echo "Then type 'dotnet run' in src/SimplCommerce.WebHost to start the app."
+	&& gulp copy-module
