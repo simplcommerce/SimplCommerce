cc32f445851a7c2220f29f626facbcc6224ce4d3
IevgenZub <euginzub@mail.ru>
2017-11-17 10:03:04 +0200

    Update simpl-build.sh

--- simpl-build.sh
+++ simpl-build.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-set -e
-
-sed -i'' -e 's|<PackageReference Include="Microsoft.EntityFrameworkCore.SqlServer" Version="2.0.0" />|<PackageReference Include="Npgsql.EntityFrameworkCore.PostgreSQL" Version="2.0.0" />|' src/SimplCommerce.WebHost/SimplCommerce.WebHost.csproj
-sed -i'' -e 's/UseSqlServer/UseNpgsql/' src/SimplCommerce.WebHost/Program.cs
-sed -i'' -e 's/UseSqlServer/UseNpgsql/' src/SimplCommerce.WebHost/Extensions/ServiceCollectionExtensions.cs
-
-rm -rf src/SimplCommerce.WebHost/Migrations/*
 
 dotnet restore && dotnet build
 
