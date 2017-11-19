e49581538b7dc9f6b462790d37f17b4971e67328
IevgenZub <euginzub@mail.ru>
2017-11-19 07:48:55 +0200

    Update simpl-build.sh

--- simpl-build.sh
+++ simpl-build.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+set -e
 
--rm -rf src/SimplCommerce.WebHost/Migrations/*
+sed -i'' -e 's|<PackageReference Include="Microsoft.EntityFrameworkCore.SqlServer" Version="2.0.0" />|<PackageReference Include="Npgsql.EntityFrameworkCore.PostgreSQL" Version="2.0.0" />|' src/SimplCommerce.WebHost/SimplCommerce.WebHost.csproj
+sed -i'' -e 's/UseSqlServer/UseNpgsql/' src/SimplCommerce.WebHost/Program.cs
+sed -i'' -e 's/UseSqlServer/UseNpgsql/' src/SimplCommerce.WebHost/Extensions/ServiceCollectionExtensions.cs
+
+rm -rf src/SimplCommerce.WebHost/Migrations/*
 
 dotnet restore && dotnet build
 
 cd src/SimplCommerce.WebHost \
 	&& npm install \
 	&& npm install --global gulp-cli \
-	&& gulp copy-modules
+	&& gulp copy-modules \
+	&& dotnet ef migrations add initialSchema \
+	&& sed -i'' -e '/using SimplCommerce.Module.*.Models;/d' Migrations/SimplDbContextModelSnapshot.cs \
+	&& sed -i'' -e '/using SimplCommerce.Module.*.Models;/d' Migrations/*_initialSchema.Designer.cs \
+	&& dotnet ef database update
+	
+echo "The database schema has been created. Please execute the src/Database/StaticData_Postgres.sql to insert static data."
+echo "Then type 'dotnet run' in src/SimplCommerce.WebHost to start the app."
