60cef7edd3e710cd111c9e5336c897b5860507db
Thien Nguyen Le Quynh <nlqthien@gmail.com>
2017-12-05 06:52:35 +0700

    #307 countries setting (#314)

--- src/Modules/SimplCommerce.Module.Core/Controllers/CountryApiController.cs
+++ src/Modules/SimplCommerce.Module.Core/Controllers/CountryApiController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SimplCommerce.Infrastructure.Data;
 using SimplCommerce.Module.Core.Models;
+using SimplCommerce.Infrastructure.Web.SmartTable;
+using SimplCommerce.Module.Core.ViewModels;
 
 namespace SimplCommerce.Module.Core.Controllers
 {
@@ -32,6 +34,53 @@ namespace SimplCommerce.Module.Core.Controllers
                     Name = x.Name
                 }).ToListAsync();
             return Json(countries);
+        }
+
+        [HttpPost("grid")]
+        public IActionResult List([FromBody] SmartTableParam param)
+        {
+            var query = _countryRepository.Query();
+
+            if (param.Search.PredicateObject != null)
+            {
+                dynamic search = param.Search.PredicateObject;
+
+                if (search.Name != null)
+                {
+                    string name = search.Name;
+                    query = query.Where(x => x.Name.Contains(name));
+                }
+            }
+
+            var countries = query.ToSmartTableResult(
+                param,
+                c => new
+                {
+                    c.Id,
+                    c.Name,
+                    c.IsShippingEnabled
+                });
+
+            return Json(countries);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(long id, [FromBody] CountryForm model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var country = await _countryRepository.Query().FirstOrDefaultAsync(x => x.Id == id);
+            if(country == null)
+            {
+                return NotFound();
+            }
+
+            country.IsShippingEnabled = model.IsShippingEnabled;
+            await _countryRepository.SaveChangesAsync();
+            return Accepted();
         }
     }
 }
---
+++ src/Modules/SimplCommerce.Module.Core/ViewModels/CountryForm.cs
@@ -0,0 +1,11 @@
+﻿namespace SimplCommerce.Module.Core.ViewModels
+{
+    public class CountryForm
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsShippingEnabled { get; set; }
+    }
+}
