ba76a8e8fd42d7802a2323e5a1fd1b783e369812
Ricardo Mauro <ricardo.demauro@gmail.com>
2017-12-13 19:52:14 -0200

    implementation of azure blob storage as simplcommerce storage.

---
+++ src/Modules/SimplCommerce.Module.StorageAzureBlobStorage/AzureBlobMediaService.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.RetryPolicies;
+using SimplCommerce.Module.Core.Models;
+using SimplCommerce.Module.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplCommerce.Module.StorageAzureBlobStorage
+{
+    public class AzureBlobMediaService : IMediaService
+    {
+        private string _storageConnectionString;
+        private string _storageContainer;
+        private bool _retryOnFailure;
+        private string _publicEndpoint;
+
+        public AzureBlobMediaService(IConfiguration configuration)
+        {
+            _storageConnectionString = configuration.GetValue("Azure:Blob:StorageConnectionString", string.Empty);
+            _storageContainer = configuration.GetValue("Azure:Blob:StorageContainer", string.Empty);
+            _retryOnFailure = configuration.GetValue("Azure:Blob:RetryOnFailure", false);
+            _publicEndpoint = configuration["Azure:Blob:PublicEndpoint"];
+
+            Contract.Requires(string.IsNullOrWhiteSpace(_storageConnectionString));
+            Contract.Requires(string.IsNullOrWhiteSpace(_storageContainer));
+        }
+
+        public Task DeleteMediaAsync(Media media)
+        {
+            return DeleteMediaAsync(media.FileName);
+        }
+
+        public async Task DeleteMediaAsync(string fileName)
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+                throw new ArgumentNullException(nameof(fileName));
+
+            var blob = await CreateBlockBlob(fileName);
+            await blob.DeleteIfExistsAsync();
+        }
+
+        public string GetMediaUrl(Media media)
+        {
+            return GetMediaUrl(media.FileName);
+        }
+
+        public string GetMediaUrl(string fileName)
+        {
+            return string.Concat(_publicEndpoint, fileName);
+        }
+
+        public string GetThumbnailUrl(Media media)
+        {
+            return string.Concat(_publicEndpoint, media.FileName);
+        }
+
+        public async Task SaveMediaAsync(Stream mediaBinaryStream, string fileName, string mimeType = null)
+        {
+            if (mediaBinaryStream == null)
+                throw new ArgumentNullException(nameof(mediaBinaryStream));
+
+            if (string.IsNullOrWhiteSpace(fileName))
+                throw new ArgumentNullException(nameof(fileName));
+
+            var blob = await CreateBlockBlob(fileName);
+
+            if (!string.IsNullOrEmpty(mimeType))
+                blob.Properties.ContentType = mimeType;
+
+            await blob.UploadFromStreamAsync(mediaBinaryStream, mediaBinaryStream.Length);
+        }
+
+        private IRetryPolicy GetRetryPolicy()
+        {
+            if (_retryOnFailure)
+                return new LinearRetry();
+
+            return new NoRetry();
+        }
+
+        private async Task<CloudBlockBlob> CreateBlockBlob(string blobName)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(_storageConnectionString);
+
+            CloudBlobClient serviceClient = storageAccount.CreateCloudBlobClient();
+
+            var container = serviceClient.GetContainerReference(_storageContainer);
+
+            BlobRequestOptions requestOptions = new BlobRequestOptions() { RetryPolicy = GetRetryPolicy() };
+            await container.CreateIfNotExistsAsync(requestOptions, null);
+            return container.GetBlockBlobReference(blobName);
+        }
+    }
+}
---
+++ src/Modules/SimplCommerce.Module.StorageAzureBlobStorage/ModuleInitializer.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SimplCommerce.Infrastructure;
+using SimplCommerce.Module.Core.Services;
+
+namespace SimplCommerce.Module.StorageAzureBlobStorage
+{
+    public class ModuleInitializer : IModuleInitializer
+    {
+        public void Init(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<IMediaService, AzureBlobMediaService>();
+        }
+    }
+}
