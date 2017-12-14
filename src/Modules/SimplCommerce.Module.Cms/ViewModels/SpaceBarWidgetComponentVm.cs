9815b8b5c3f0e452b5347c0157b057c3342560db
IBERMATICA\MASABESA <s.saleem@ibermatica.com>
2017-12-14 10:37:14 +0100

    SpaceBar Widget

---
+++ src/Modules/SimplCommerce.Module.Cms/ViewModels/SpaceBarWidgetComponentVm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimplCommerce.Module.Cms.ViewModels
+{
+    public class SpaceBarWidgetComponentVm
+    {
+        public long Id { get; set; }
+        public string WidgetName { get; set; }
+        public List<SpaceBarWidgetSetting> Items { get; set; }
+    }
+}
