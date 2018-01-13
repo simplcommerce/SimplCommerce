1d65dd41626d866b9d08935b0b5e164b21ae6dee
Do Quang Huy <dohuy91@gmail.com>
2018-01-13 17:00:22 +0700

    Implement #262: API wrapper exception middleware

---
+++ src/SimplCommerce.Infrastructure/Web/Middlewares/ApiErrorHandlerOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimplCommerce.Infrastructure.Web.Middlewares
+{
+    public class ApiErrorHandlerOptions
+    {
+        public List<string> HandledPaths { get; set; } = new List<string> { "/api" };
+    }
+}
