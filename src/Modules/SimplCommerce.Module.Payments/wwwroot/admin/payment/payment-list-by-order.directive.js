f30f77d121cc3ead157bb83f301059ee8a13cd9a
Thien Nguyen <nlqthien@gmail.com>
2018-01-24 02:12:28 +0700

    adde payment list to order details

---
+++ src/Modules/SimplCommerce.Module.Payments/wwwroot/admin/payment/payment-list-by-order.directive.js
@@ -0,0 +1,33 @@
+﻿(function() {
+    angular
+        .module('simplAdmin.payments')
+        .directive('paymentListByOrder', paymentListByOrder);
+
+    function paymentListByOrder() {
+        var directive = {
+            restrict: 'E',
+            templateUrl: 'modules/payments/admin/payment/payment-list-by-order.directive.html',
+            scope: {},
+            bindToController: {
+                orderId: '='
+            },
+            controller: PaymentListByOrderCtrl,
+            controllerAs: 'vm'
+        };
+
+        return directive;
+    }
+
+    /* @ngInject */
+    function PaymentListByOrderCtrl(paymentService, translateService) {
+        var vm = this;
+        vm.translate = translateService;
+        vm.payments = [];
+
+        vm.$onInit = function () {
+            paymentService.getPaymentsByOrder(vm.orderId).then(function (result) {
+                vm.payments = result.data;
+            });
+        };
+    }
+})();
---
+++ src/Modules/SimplCommerce.Module.Payments/wwwroot/admin/payment/payment-service.js
@@ -0,0 +1,18 @@
+﻿/*global angular*/
+(function () {
+    angular
+        .module('simplAdmin.payments')
+        .factory('paymentService', paymentService);
+
+    /* @ngInject */
+    function paymentService($http) {
+        var service = {
+            getPaymentsByOrder: getPaymentsByOrder
+        };
+        return service;
+
+        function getPaymentsByOrder(orderId) {
+            return $http.get('api/orders/' + orderId + '/payments');
+        }
+    }
+})();
