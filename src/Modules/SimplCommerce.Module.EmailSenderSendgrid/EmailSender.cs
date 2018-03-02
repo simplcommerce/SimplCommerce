34ee0a710cd4b56af69e04105c2f7a288d0d7df7
Jérôme Ducobu (Jaydee Event) <jerome@jaydeeevent.onmicrosoft.com>
2018-03-02 21:27:23 +0100

    Implements #414

--- src/Modules/SimplCommerce.Module.EmailSenderSendgrid/EmailSender.cs
+++ src/Modules/SimplCommerce.Module.EmailSenderSendgrid/EmailSender.cs
@@ -1,13 +1,42 @@
-﻿using System.Threading.Tasks;
+﻿using System.Diagnostics.Contracts;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using SimplCommerce.Module.Core.Services;
 
 namespace SimplCommerce.Module.EmailSenderSendgrid
 {
     public class EmailSender : IEmailSender
     {
-        public Task SendEmailAsync(string email, string subject, string message, bool isHtml = false)
+        private readonly string _apiKey;
+        private readonly string _from;
+        public EmailSender(IConfiguration configuration)
         {
-            throw new System.NotImplementedException();
+            _apiKey = configuration.GetValue<string>("SendGrid:Email:ApiKey");
+            _from = configuration.GetValue<string>("SendGrid:Email:From");
+
+            Contract.Requires(string.IsNullOrWhiteSpace(_apiKey));
+            Contract.Requires(string.IsNullOrWhiteSpace(_from));
+        }
+        public async Task SendEmailAsync(string email, string subject, string message, bool isHtml = false)
+        {
+            Contract.Requires(string.IsNullOrWhiteSpace(email));
+            Contract.Requires(string.IsNullOrWhiteSpace(subject));
+            Contract.Requires(string.IsNullOrWhiteSpace(message));
+
+            var sendGridMessage = new SendGridMessage();
+
+            sendGridMessage.From = new EmailAddress(_from);
+            sendGridMessage.AddTo(new EmailAddress(email));
+            sendGridMessage.Subject = subject;
+            sendGridMessage.HtmlContent = isHtml ? message : "";
+            sendGridMessage.PlainTextContent = isHtml ? Regex.Replace(message, "<[^>]*>", "") : message;
+
+            var client = new SendGridClient(_apiKey);
+            await client.SendEmailAsync(sendGridMessage);
         }
     }
 }
