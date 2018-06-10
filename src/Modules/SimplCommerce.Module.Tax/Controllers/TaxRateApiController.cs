d5f7710e9cfcccaa14017eb68fcc2c2e059b15fc
Thien Nguyen <nlqthien@gmail.com>
2018-06-10 23:50:29 +0700

    #433 Import/Export TaxRate

--- src/Modules/SimplCommerce.Module.Tax/Controllers/TaxRateApiController.cs
+++ src/Modules/SimplCommerce.Module.Tax/Controllers/TaxRateApiController.cs
@@ -1,9 +1,12 @@
 ﻿using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using SimplCommerce.Infrastructure;
 using SimplCommerce.Infrastructure.Data;
+using SimplCommerce.Module.Core.Models;
 using SimplCommerce.Module.Tax.Models;
 using SimplCommerce.Module.Tax.ViewModels;
 
@@ -14,10 +17,12 @@ namespace SimplCommerce.Module.Tax.Controllers
     public class TaxRateApiController : Controller
     {
         private readonly IRepository<TaxRate> _taxRateRepository;
+        private readonly IRepository<StateOrProvince> _stateOrProvinceRepository;
 
-        public TaxRateApiController(IRepository<TaxRate> taxRateRepository)
+        public TaxRateApiController(IRepository<TaxRate> taxRateRepository, IRepository<StateOrProvince> stateOrProvinceRepository)
         {
             _taxRateRepository = taxRateRepository;
+            _stateOrProvinceRepository = stateOrProvinceRepository;
         }
 
         public async Task<IActionResult> Get()
@@ -27,7 +32,6 @@ namespace SimplCommerce.Module.Tax.Controllers
                 .Select(x => new
                 {
                     x.Id,
-                    x.Name,
                     TagClassName = x.TaxClass.Name,
                     CountryName = x.Country.Name,
                     StateOrProvinceName = x.StateOrProvince.Name,
@@ -36,6 +40,28 @@ namespace SimplCommerce.Module.Tax.Controllers
                 })
                 .ToListAsync();
             return Json(taxRates);
+        }
+
+        [HttpGet("export")]
+        public async Task<IActionResult> Export()
+        {
+            var taxRates = await _taxRateRepository
+                .Query()
+                .Select(x => new TaxRateImport
+                {
+                    TaxClassId = x.TaxClassId,
+                    CountryId = x.CountryId,
+                    StateOrProvinceName = x.StateOrProvince.Name,
+                    ZipCode = x.ZipCode,
+                    Rate = x.Rate
+                })
+                .ToListAsync();
+
+            var csvString = CsvConverter.ExportCsv(taxRates);
+            var csvBytes = Encoding.UTF8.GetBytes(csvString);
+            // MS Excel need the BOM to display UTF8 Correctly
+            var csvBytesWithUTF8BOM = Encoding.UTF8.GetPreamble().Concat(csvBytes).ToArray();
+            return File(csvBytesWithUTF8BOM, "text/csv", "tax-rates.csv");
         }
 
         [HttpGet("{id}")]
@@ -50,7 +76,6 @@ namespace SimplCommerce.Module.Tax.Controllers
             var model = new TaxRateForm
             {
                 Id = taxRate.Id,
-                Name = taxRate.Name,
                 TaxClassId = taxRate.TaxClassId,
                 CountryId = taxRate.CountryId,
                 StateOrProvinceId = taxRate.StateOrProvinceId,
@@ -68,7 +93,6 @@ namespace SimplCommerce.Module.Tax.Controllers
             {
                 var tagRate = new TaxRate
                 {
-                    Name = model.Name,
                     TaxClassId = model.TaxClassId,
                     CountryId = model.CountryId,
                     StateOrProvinceId = model.StateOrProvinceId,
@@ -95,7 +119,6 @@ namespace SimplCommerce.Module.Tax.Controllers
                     return NotFound();
                 }
 
-                taxRate.Name = model.Name;
                 taxRate.TaxClassId = model.TaxClassId;
                 taxRate.CountryId = model.CountryId;
                 taxRate.StateOrProvinceId = model.StateOrProvinceId;
@@ -125,9 +148,38 @@ namespace SimplCommerce.Module.Tax.Controllers
             }
             catch (DbUpdateException)
             {
-                return BadRequest(new { Error = $"The tax rate {taxRate.Name} can't not be deleted because it is referenced by other tables" });
+                return BadRequest(new { Error = $"The tax rate {taxRate.Id} can't not be deleted because it is referenced by other tables" });
             }
 
+            return NoContent();
+        }
+
+        [HttpPost("import")]
+        public async Task<IActionResult> Import(TaxRateImportForm model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var inputStream = model.CsvFile.OpenReadStream();
+            var records = CsvConverter.ReadCsvStream<TaxRateImport>(model.CsvFile.OpenReadStream(), model.IncludeHeader, model.CsvDelimiter);
+
+            foreach(var record in records)
+            {
+                var stateOrProvince = _stateOrProvinceRepository.Query().FirstOrDefault(x => x.Name == record.StateOrProvinceName);
+                var taxRate = new TaxRate
+                {
+                    TaxClassId = record.TaxClassId,
+                    CountryId = record.CountryId,
+                    StateOrProvince = stateOrProvince,
+                    ZipCode = record.ZipCode,
+                    Rate = record.Rate
+                };
+                _taxRateRepository.Add(taxRate);
+            }
+
+            await _taxRateRepository.SaveChangesAsync();
             return NoContent();
         }
     }
---
+++ src/SimplCommerce.Infrastructure/CsvConverter.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace SimplCommerce.Infrastructure
+{
+    public static class CsvConverter
+    {
+        public static IList<T> ReadCsvStream<T>(Stream stream, bool skipFirstLine = true, string csvDelimiter = ",") where T : new()
+        {
+            var records = new List<T>();
+            var reader = new StreamReader(stream);
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(csvDelimiter.ToCharArray());
+                if (skipFirstLine)
+                {
+                    skipFirstLine = false;
+                }
+                else
+                {
+                    var itemTypeInGeneric = records.GetType().GetTypeInfo().GenericTypeArguments[0];
+                    var item = new T();
+                    var properties = item.GetType().GetProperties();
+                    for (int i = 0; i < values.Length; i++)
+                    {
+                        properties[i].SetValue(item, Convert.ChangeType(values[i], properties[i].PropertyType), null);
+                    }
+
+                    records.Add(item);
+                }
+            }
+
+            return records;
+        }
+
+        public static string ExportCsv<T>(IList<T> data, bool includeHeader = true, string csvDelimiter = ",") where T : new()
+        {
+            var type = data.GetType();
+            Type itemType;
+
+            if (type.GetGenericArguments().Length > 0)
+            {
+                itemType = type.GetGenericArguments()[0];
+            }
+            else
+            {
+                itemType = type.GetElementType();
+            }
+
+            var stringWriter = new StringWriter();
+
+            if (includeHeader)
+            {
+                stringWriter.WriteLine(
+                    string.Join<string>(
+                        csvDelimiter, itemType.GetProperties().Select(x => x.Name)
+                    )
+                );
+            }
+
+            foreach (var obj in data)
+            {
+                var vals = obj.GetType().GetProperties().Select(pi => new
+                    {
+                        Value = pi.GetValue(obj, null)
+                    }
+                );
+
+                string line = string.Empty;
+                foreach (var val in vals)
+                {
+                    if (val.Value != null)
+                    {
+                        var escapeVal = val.Value.ToString();
+                        //Check if the value contans a comma and place it in quotes if so
+                        if (escapeVal.Contains(","))
+                        {
+                            escapeVal = string.Concat("\"", escapeVal, "\"");
+                        }
+
+                        //Replace any \r or \n special characters from a new line with a space
+                        if (escapeVal.Contains("\r"))
+                        {
+                            escapeVal = escapeVal.Replace("\r", " ");
+                        }
+
+                        if (escapeVal.Contains("\n"))
+                        {
+                            escapeVal = escapeVal.Replace("\n", " ");
+                        }
+
+                        line = string.Concat(line, escapeVal, csvDelimiter);
+                    }
+                    else
+                    {
+                        line = string.Concat(line, string.Empty, csvDelimiter);
+                    }
+                }
+
+                stringWriter.WriteLine(line.TrimEnd(csvDelimiter.ToCharArray()));
+            }
+
+            return stringWriter.ToString();
+        }
+    }
+}
