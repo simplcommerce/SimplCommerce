0d450d007ed792f3f4a137dd35bda323a91afcf3
teeshter <teeshter@outlook.com>
2018-08-18 18:05:24 -0700

    Add Wish List module

---
+++ src/Modules/SimplCommerce.Module.WishList/Models/WishList.cs
@@ -0,0 +1,28 @@
+﻿using SimplCommerce.Infrastructure.Models;
+using SimplCommerce.Module.Core.Models;
+using System;
+using System.Collections.Generic;
+
+namespace SimplCommerce.Module.WishList.Models
+{
+    public class WishList : EntityBase
+    {
+        public WishList()
+        {
+            CreatedOn = DateTimeOffset.Now;
+            UpdatedOn = DateTimeOffset.Now;
+        }
+
+        public long UserId { get; set; }
+
+        public User User { get; set; }
+
+        public string SharingCode { get; set; }
+
+        public IList<WishListItem> Items { get; protected set; } = new List<WishListItem>();
+
+        public DateTimeOffset CreatedOn { get; set; }
+
+        public DateTimeOffset UpdatedOn { get; set; }
+    }
+}
---
+++ src/SimplCommerce.WebHost/Migrations/20180812173106_AddWishListModule.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SimplCommerce.WebHost.Migrations
+{
+    public partial class AddWishListModule : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WishList_WishList",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<long>(nullable: false),
+                    SharingCode = table.Column<string>(nullable: true),
+                    CreatedOn = table.Column<DateTimeOffset>(nullable: false),
+                    UpdatedOn = table.Column<DateTimeOffset>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WishList_WishList", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WishList_WishList_Core_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Core_User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WishList_WishListItem",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    WishListId = table.Column<long>(nullable: false),
+                    ProductId = table.Column<long>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Quantity = table.Column<int>(nullable: false),
+                    CreatedOn = table.Column<DateTimeOffset>(nullable: false),
+                    UpdatedOn = table.Column<DateTimeOffset>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WishList_WishListItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WishList_WishListItem_Catalog_Product_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Catalog_Product",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_WishList_WishListItem_WishList_WishList_WishListId",
+                        column: x => x.WishListId,
+                        principalTable: "WishList_WishList",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WishList_WishList_UserId",
+                table: "WishList_WishList",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WishList_WishListItem_ProductId",
+                table: "WishList_WishListItem",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WishList_WishListItem_WishListId",
+                table: "WishList_WishListItem",
+                column: "WishListId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WishList_WishListItem");
+
+            migrationBuilder.DropTable(
+                name: "WishList_WishList");
+        }
+    }
+}
