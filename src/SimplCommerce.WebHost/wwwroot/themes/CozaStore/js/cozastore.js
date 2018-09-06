3eb83db66063493efe717b54a65a79245b3be04c
Thien Nguyen <nlqthien@gmail.com>
2018-09-06 18:36:27 +0700

    coza theme: fix js error and update footer

--- src/SimplCommerce.WebHost/wwwroot/themes/CozaStore/js/cozastore.js
+++ src/SimplCommerce.WebHost/wwwroot/themes/CozaStore/js/cozastore.js
@@ -100,7 +100,6 @@
 
             $('.sub-menu-m').each(function () {
                 if ($(this).css('display') == 'block') {
-                    console.log('hello');
                     $(this).css('display', 'none');
                     $(arrowMainMenu).removeClass('turn-arrow-main-menu-m');
                 }
@@ -125,73 +124,6 @@
     $('.container-search-header').on('click', function (e) {
         e.stopPropagation();
     });
-
-
-    /*==================================================================
-    [ Isotope ]*/
-    var $topeContainer = $('.isotope-grid');
-    var $filter = $('.filter-tope-group');
-
-    // filter items on button click
-    $filter.each(function () {
-        $filter.on('click', 'button', function () {
-            var filterValue = $(this).attr('data-filter');
-            $topeContainer.isotope({ filter: filterValue });
-        });
-
-    });
-
-    // init Isotope
-    $(window).on('load', function () {
-        var $grid = $topeContainer.each(function () {
-            $(this).isotope({
-                itemSelector: '.isotope-item',
-                layoutMode: 'fitRows',
-                percentPosition: true,
-                animationEngine: 'best-available',
-                masonry: {
-                    columnWidth: '.isotope-item'
-                }
-            });
-        });
-    });
-
-    var isotopeButton = $('.filter-tope-group button');
-
-    $(isotopeButton).each(function () {
-        $(this).on('click', function () {
-            for (var i = 0; i < isotopeButton.length; i++) {
-                $(isotopeButton[i]).removeClass('how-active1');
-            }
-
-            $(this).addClass('how-active1');
-        });
-    });
-
-    /*==================================================================
-    [ Filter / Search product ]*/
-    $('.js-show-filter').on('click', function () {
-        $(this).toggleClass('show-filter');
-        $('.panel-filter').slideToggle(400);
-
-        if ($('.js-show-search').hasClass('show-search')) {
-            $('.js-show-search').removeClass('show-search');
-            $('.panel-search').slideUp(400);
-        }
-    });
-
-    $('.js-show-search').on('click', function () {
-        $(this).toggleClass('show-search');
-        $('.panel-search').slideToggle(400);
-
-        if ($('.js-show-filter').hasClass('show-filter')) {
-            $('.js-show-filter').removeClass('show-filter');
-            $('.panel-filter').slideUp(400);
-        }
-    });
-
-
-
 
     /*==================================================================
     [ Cart ]*/
@@ -277,7 +209,5 @@
     $('.js-hide-modal1').on('click', function () {
         $('.js-modal1').removeClass('show-modal1');
     });
-
-
 
 })(jQuery);
