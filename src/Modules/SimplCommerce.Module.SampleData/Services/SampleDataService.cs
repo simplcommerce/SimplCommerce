766081c6bd5a5b7b42a5c5a8ca226548d76a6365
Thien Nguyen <nlqthien@gmail.com>
2018-09-07 03:54:24 +0700

    #71 support MySql using Pomelo.EntityFrameworkCore.MySql

--- src/Modules/SimplCommerce.Module.SampleData/Services/SampleDataService.cs
+++ src/Modules/SimplCommerce.Module.SampleData/Services/SampleDataService.cs
@@ -23,11 +23,16 @@ namespace SimplCommerce.Module.SampleData.Services
         {
             var usePostgres = _sqlRepository.GetDbConnectionType() == "Npgsql.NpgsqlConnection";
             var useSQLite = _sqlRepository.GetDbConnectionType() == "Microsoft.Data.Sqlite.SqliteConnection";
+            var useMySql = _sqlRepository.GetDbConnectionType().Contains("MySql", System.StringComparison.InvariantCultureIgnoreCase);
             var sampleContentFolder = Path.Combine(GlobalConfiguration.ContentRootPath, "Modules", "SimplCommerce.Module.SampleData", "SampleContent", model.Industry);
 
-            var filePath = usePostgres ? Path.Combine(sampleContentFolder, "ResetToSampleData_Postgres.sql") : useSQLite ? Path.Combine(sampleContentFolder, "ResetToSampleData_SQLite.sql") : Path.Combine(sampleContentFolder, "ResetToSampleData.sql");
+            var filePath = 
+                usePostgres ? Path.Combine(sampleContentFolder, "ResetToSampleData_Postgres.sql") :
+                useSQLite ? Path.Combine(sampleContentFolder, "ResetToSampleData_SQLite.sql") :
+                useMySql ? Path.Combine(sampleContentFolder, "ResetToSampleData_MySql.sql") :
+                Path.Combine(sampleContentFolder, "ResetToSampleData.sql");
             var lines = File.ReadLines(filePath);
-            var commands = usePostgres || useSQLite ? _sqlRepository.PostgresCommands(lines) : _sqlRepository.ParseCommand(lines);
+            var commands = usePostgres || useSQLite || useMySql ? _sqlRepository.PostgresCommands(lines) : _sqlRepository.ParseCommand(lines);
             _sqlRepository.RunCommands(commands);
 
            await CopyImages(sampleContentFolder);
