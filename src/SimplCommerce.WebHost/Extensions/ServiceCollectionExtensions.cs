8d6d097cf6f1735a9e2e99d4246381881a4765d7
Rafsanul Hasan <rafsanulhasan@outlook.com>
2018-09-19 14:16:46 +0600

    View Location Expanders and Compilation Reference are not needed and thus removed

--- src/SimplCommerce.WebHost/Extensions/ServiceCollectionExtensions.cs
+++ src/SimplCommerce.WebHost/Extensions/ServiceCollectionExtensions.cs
@@ -90,13 +90,13 @@ namespace SimplCommerce.WebHost.Extensions
                         o.ModelBinderProviders.Insert(0, new InvariantDecimalModelBinderProvider(loggerFactory));
                     }
                 })
-                .AddRazorOptions(o =>
-                {
-                    foreach (var module in modules)
-                    {
-                        o.AdditionalCompilationReferences.Add(MetadataReference.CreateFromFile(module.Assembly.Location));
-                    }
-                })
+                //.AddRazorOptions(o =>
+                //{
+                //    foreach (var module in modules)
+                //    {
+                //        o.AdditionalCompilationReferences.Add(MetadataReference.CreateFromFile(module.Assembly.Location));
+                //    }
+                //})
                 .AddViewLocalization()
                 .AddDataAnnotationsLocalization()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_1); ;
