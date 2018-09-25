f29e48adc5cd135db2d86afb12331d65c3c0e24b
Thien Nguyen <nlqthien@gmail.com>
2018-09-25 22:15:14 +0700

    copy module by msbuild task

---
+++ build/SimplCommerce.MSBuildTasks/CopyModules.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace SimplCommerce.MSBuildTasks
+{
+    public class CopyModules : Microsoft.Build.Utilities.Task
+    {
+        public override bool Execute()
+        {
+            var moduleString = "";
+            var modules = new List<Module>();
+
+            using (var ms = new MemoryStream(Encoding.UTF8.GetBytes(moduleString)))
+            {
+                DataContractJsonSerializer ser = new DataContractJsonSerializer(modules.GetType());
+                modules = ser.ReadObject(ms) as List<Module>;
+            }
+
+            return true;
+        }
+    }
+}
