65ab297c9b19d12b710477452746583e8fd8a5c3
Nguyễn Thanh Xuân <thanhxuanhd007@gmail.com>
2018-10-01 00:16:54 +0700

    Show a tree of categories in product filters #491

---
+++ src/Modules/SimplCommerce.Module.Search/Components/CategoriesTreeViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimplCommerce.Module.Search.ViewModels;
+
+namespace SimplCommerce.Module.Search.Components
+{
+    public class CategoriesTreeViewComponent : ViewComponent
+    {
+        #region Ctor
+
+        public CategoriesTreeViewComponent()
+        {
+        }
+
+        #endregion Ctor
+
+        #region Action
+
+        public IViewComponentResult Invoke(SearchResult searchResult)
+        {
+            return View(searchResult);
+        }
+
+        #endregion Action
+    }
+}
