a1849659e7f22617f158438c844f675544b2cac4
Thien Nguyen <nlqthien@gmail.com>
2018-10-09 16:43:04 +0700

    token auth - correct setting keys

--- src/Modules/SimplCommerce.Module.Core/Services/TokenService.cs
+++ src/Modules/SimplCommerce.Module.Core/Services/TokenService.cs
@@ -19,14 +19,14 @@ namespace SimplCommerce.Module.Core.Services
         }
         public string GenerateAccessToken(IEnumerable<Claim> claims)
         {
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Authentication:Jwt:Key"]));
 
             var jwtToken = new JwtSecurityToken(
-                issuer: _configuration["Jwt:Issuer"],
+                issuer: _configuration["Authentication:Jwt:Issuer"],
                 audience: "Anyone",
                 claims: claims,
                 notBefore: DateTime.UtcNow,
-                expires: DateTime.UtcNow.AddMinutes(int.Parse(_configuration["Jwt:AccessTokenDurationInMinutes"])),
+                expires: DateTime.UtcNow.AddMinutes(int.Parse(_configuration["Authentication:Jwt:AccessTokenDurationInMinutes"])),
                 signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
             );
 
@@ -49,9 +49,9 @@ namespace SimplCommerce.Module.Core.Services
             {
                 ValidateAudience = false,
                 ValidateIssuer = true,
-                ValidIssuer = _configuration["Jwt:Issuer"],
+                ValidIssuer = _configuration["Authentication:Jwt:Issuer"],
                 ValidateIssuerSigningKey = true,
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"])),
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Authentication:Jwt:Key"])),
                 ValidateLifetime = false //in this case, we don't care about the token's expiration date
             };
 
