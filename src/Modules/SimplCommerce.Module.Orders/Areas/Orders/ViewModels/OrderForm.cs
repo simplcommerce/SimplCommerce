ecfd8c5e66b0207367757e8a47cf52b457f6322f
codingdefined <codingdefined@gmail.com>
2018-11-08 09:08:43 +0530

    Added OrderForm for Creating Form

---
+++ src/Modules/SimplCommerce.Module.Orders/Areas/Orders/ViewModels/OrderForm.cs
@@ -0,0 +1,10 @@
+﻿namespace SimplCommerce.Module.Orders.Areas.Orders.ViewModels
+{
+    public class OrderForm
+    {
+        public long Id { get; set; }
+        public long CustomerID { get; set; }
+        public string PaymentMethod { get; set; }
+        public decimal PaymentFeeAmount { get; set; }
+    }
+}
