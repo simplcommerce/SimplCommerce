11fb19180e26b62f5545bd85a9dcd077c1dc14c2
adrianBasa <adryangolubey@yahoo.com>
2018-11-13 23:30:11 +0200

    Add files via upload

---
+++ ReviewViewComponent.cs
@@ -0,0 +1,97 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SimplCommerce.Infrastructure.Data;
+using SimplCommerce.Infrastructure.Web;
+using SimplCommerce.Module.Core.Extensions;
+using SimplCommerce.Module.Orders.Models;
+using SimplCommerce.Module.Reviews.Areas.Reviews.ViewModels;
+using SimplCommerce.Module.Reviews.Models;
+
+namespace SimplCommerce.Module.Reviews.Areas.Reviews.Components
+{
+    public class ReviewViewComponent : ViewComponent
+    {  
+        private readonly IRepository<Review> _reviewRepository;
+        private readonly IWorkContext _workContext;
+        private readonly IRepository<Order> _orderRepository;
+
+        public ReviewViewComponent(IRepository<Review> reviewRepository, IWorkContext workContext, IRepository<Order> orderRepository )
+        {
+            _reviewRepository = reviewRepository;
+            _workContext = workContext;
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(long entityId, string entityTypeId)
+        {
+            var model = new ReviewVm
+            {
+                EntityId = entityId,
+                EntityTypeId = entityTypeId
+            };
+
+            model.Items.Data = await _reviewRepository
+                .Query()
+                .Where(x => (x.EntityId == entityId) && (x.EntityTypeId == entityTypeId) && (x.Status == ReviewStatus.Approved))
+                .OrderByDescending(x => x.CreatedOn)
+                .Select(x => new ReviewItem
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Rating = x.Rating,
+                    Comment = x.Comment,
+                    ReviewerName = x.ReviewerName,
+                    CreatedOn = x.CreatedOn,
+                    Replies = x.Replies
+                        .Where(r => r.Status == ReplyStatus.Approved)
+                        .OrderByDescending(r => r.CreatedOn)
+                        .Select(r => new ViewModels.Reply
+                        {
+                            Comment = r.Comment,
+                            ReplierName = r.ReplierName,
+                            CreatedOn = r.CreatedOn
+                        })
+                        .ToList()
+                }).ToListAsync();
+
+            model.ReviewsCount = model.Items.Data.Count;
+            model.Rating1Count = model.Items.Data.Count(x => x.Rating == 1);
+            model.Rating2Count = model.Items.Data.Count(x => x.Rating == 2);
+            model.Rating3Count = model.Items.Data.Count(x => x.Rating == 3);
+            model.Rating4Count = model.Items.Data.Count(x => x.Rating == 4);
+            model.Rating5Count = model.Items.Data.Count(x => x.Rating == 5);
+
+            if (User.Identity.IsAuthenticated == true)
+            {
+                var user = await _workContext.GetCurrentUser();
+                model.LoggedUserName = user.FullName;
+
+                var currentUserOrder = _orderRepository
+                    .Query()
+                    .Where(x => x.CreatedById == user.Id);
+                var userOrderedProduct = currentUserOrder
+                    .Where(x => x.OrderItems.Where(y =>y.ProductId == entityId).Count() > 0);
+
+            
+   
+                 model.CanReview = userOrderedProduct.Any() ? true : false;
+
+                }
+
+
+            
+
+            else
+            {
+                model.LoggedUserName = string.Empty;
+                model.CanReview = false;
+            }
+
+
+            return View(this.GetViewPath(), model);
+        }
+    }
+}
+
---
+++ ReviewVm.cs
@@ -0,0 +1,56 @@
+﻿using cloudscribe.Pagination.Models;
+
+namespace SimplCommerce.Module.Reviews.Areas.Reviews.ViewModels
+{
+    public class ReviewVm
+    {
+        public long EntityId { get; set; }
+
+        public string EntityTypeId { get; set; }
+
+        public string EntityName { get; set; }
+
+        public string EntitySlug { get; set; }
+
+        public bool CanReview { get; set; }
+
+        public string LoggedUserName { get; set; }
+
+        public int ReviewsCount { get; set; }
+
+        public double RatingAverage
+        {
+            get
+            {
+                if (ReviewsCount > 0)
+                {
+                    return ((1 * Rating1Count) + (2 * Rating2Count) + (3 * Rating3Count) + (4 * Rating4Count) + (5 * Rating5Count)) / (double)ReviewsCount;
+                }
+
+                return 0;
+            }
+        }
+
+        public double GetRatingPercent(int rateCount)
+        {
+            if (ReviewsCount > 0)
+            {
+                return (double)rateCount/ReviewsCount*100;
+            }
+
+            return 0;
+        }
+
+        public int Rating1Count { get; set; }
+
+        public int Rating2Count { get; set; }
+
+        public int Rating3Count { get; set; }
+
+        public int Rating4Count { get; set; }
+
+        public int Rating5Count { get; set; }
+
+        public PagedResult<ReviewItem> Items { get; set; } = new PagedResult<ReviewItem>();
+    }
+}
