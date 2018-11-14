e602fd9afa19fa73bfb6a9a8ad52003e17a2dfcc
Thien Nguyen <nlqthien@gmail.com>
2018-11-14 21:53:45 +0700

    #654 show all information of addresses

--- src/Modules/SimplCommerce.Module.Orders/Areas/Orders/Controllers/OrderApiController.cs
+++ src/Modules/SimplCommerce.Module.Orders/Areas/Orders/Controllers/OrderApiController.cs
@@ -178,7 +178,8 @@ namespace SimplCommerce.Module.Orders.Areas.Orders.Controllers
                 ShippingAddress = new ShippingAddressVm
                 {
                     AddressLine1 = order.ShippingAddress.AddressLine1,
-                    AddressLine2 = order.ShippingAddress.AddressLine2,
+                    CityName = order.ShippingAddress.City,
+                    ZipCode = order.ShippingAddress.ZipCode,
                     ContactName = order.ShippingAddress.ContactName,
                     DistrictName = order.ShippingAddress.District?.Name,
                     StateOrProvinceName = order.ShippingAddress.StateOrProvince.Name,
