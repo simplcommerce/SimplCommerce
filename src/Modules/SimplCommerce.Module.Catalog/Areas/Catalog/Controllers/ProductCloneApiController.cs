bd78544fa803701b399cd6d514ab296373ce6da5
Thien Nguyen <nlqthien@gmail.com>
2018-12-02 22:50:11 +0700

    #5 clone product: productService.Create instead of repository

--- src/Modules/SimplCommerce.Module.Catalog/Areas/Catalog/Controllers/ProductCloneApiController.cs
+++ src/Modules/SimplCommerce.Module.Catalog/Areas/Catalog/Controllers/ProductCloneApiController.cs
@@ -3,10 +3,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SimplCommerce.Infrastructure.Data;
 using SimplCommerce.Module.Catalog.Areas.Catalog.ViewModels;
 using SimplCommerce.Module.Catalog.Models;
+using SimplCommerce.Module.Catalog.Services;
 using SimplCommerce.Module.Core.Extensions;
+using SimplCommerce.Module.Core.Models;
+using SimplCommerce.Module.Core.Services;
 
 namespace SimplCommerce.Module.Catalog.Areas.Catalog.Controllers
 {
@@ -16,11 +20,15 @@ namespace SimplCommerce.Module.Catalog.Areas.Catalog.Controllers
     public class ProductCloneApiController : Controller
     {
         private readonly IRepository<Product> _productRepository;
+        private readonly IProductService _productService;
+        private readonly IMediaService _mediaService;
         private readonly IWorkContext _workContext;
 
-        public ProductCloneApiController(IRepository<Product> productRepository, IWorkContext workContext)
+        public ProductCloneApiController(IRepository<Product> productRepository, IProductService productService, IMediaService mediaService, IWorkContext workContext)
         {
             _productRepository = productRepository;
+            _productService = productService;
+            _mediaService = mediaService;
             _workContext = workContext;
         }
 
@@ -38,30 +46,51 @@ namespace SimplCommerce.Module.Catalog.Areas.Catalog.Controllers
         }
 
         [HttpPost]
-        [Authorize(Roles = "admin")]
         public async Task<IActionResult> Post([FromBody] ProductCloneFormVm model)
         {
             if (ModelState.IsValid)
             {
                 var currentUser = await _workContext.GetCurrentUser();
-                var producctId = model.Id;
-                var productNmae = model.Name;
-                var product = _productRepository.Query().FirstOrDefault(x => x.Id == producctId);
-                if (product != null)
-                {
-                    var newProduct = product.Clone();
-                    newProduct.Name = productNmae;
-                    newProduct.CreatedById = currentUser.Id;
-                    newProduct.LatestUpdatedById = currentUser.Id;
-                    newProduct.CreatedOn = DateTimeOffset.Now;
-                    newProduct.LatestUpdatedOn = DateTimeOffset.Now;
-                    _productRepository.Add(newProduct);
-                    _productRepository.SaveChanges();
+                var product = _productRepository.Query()
+                    .Include(x => x.AttributeValues)
+                    .Include(x => x.Categories)
+                    .FirstOrDefault(x => x.Id == model.Id);
 
-                    return CreatedAtAction(nameof(Get), new { id = newProduct.Id }, null);
+                if (product == null)
+                {
+                    return NotFound();
                 }
+
+                var newProduct = product.Clone();
+                newProduct.Name = model.Name;
+                newProduct.Slug = model.Slug;
+                newProduct.CreatedById = currentUser.Id;
+                newProduct.LatestUpdatedById = currentUser.Id;
+                newProduct.CreatedOn = DateTimeOffset.Now;
+                newProduct.LatestUpdatedOn = DateTimeOffset.Now;
+
+                var productPriceHistory = CreatePriceHistory(currentUser, newProduct);
+                newProduct.PriceHistories.Add(productPriceHistory);
+
+                _productService.Create(newProduct);
+                return Created($"api/products/{newProduct.Id}", new { Id = newProduct.Id });
             }
+
             return BadRequest(ModelState);
+        }
+
+        private static ProductPriceHistory CreatePriceHistory(User loginUser, Product product)
+        {
+            return new ProductPriceHistory
+            {
+                CreatedBy = loginUser,
+                Product = product,
+                Price = product.Price,
+                OldPrice = product.OldPrice,
+                SpecialPrice = product.SpecialPrice,
+                SpecialPriceStart = product.SpecialPriceStart,
+                SpecialPriceEnd = product.SpecialPriceEnd
+            };
         }
     }
 }
