cb5095bf50ce0200e3e5444426c9e9c5db821b37
农志升 <nongzhsh@outlook.com>
2018-12-10 20:36:11 +0800

    Add hangfirejobs module

---
+++ src/Modules/SimplCommerce.Module.HangfireJobs/Internal/HangfireConfigureOptions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+
+namespace SimplCommerce.Module.HangfireJobs.Internal
+{
+    /// <summary>
+    /// Defines options for the Hangfire configure.
+    /// </summary>
+    public class HangfireConfigureOptions
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether the Hangfire background server should be enabled.
+        /// </summary>
+        public bool EnableServer { get; set; } = true;
+
+        /// <summary>
+        /// Gets or sets options for the Hangfire dashboard.
+        /// </summary>
+        public DasbhoardOptions Dasbhoard { get; set; } = new DasbhoardOptions();
+
+        /// <summary>
+        /// Defines options for the Hangfire dashboard.
+        /// </summary>
+        public class DasbhoardOptions
+        {
+            /// <summary>
+            /// Gets or sets a value indicating whether the Hangfire dashboard should be enabled.
+            /// </summary>
+            public bool Enabled { get; set; } = true;
+
+            /// <summary>
+            /// Gets or sets a value indicating whether the Hangfire dashboard IP-based authorization filter should be enabled.
+            /// </summary>
+            public bool EnableAuthorization { get; set; } = true;
+
+            /// <summary>
+            /// Gets or sets a callback which gets invoked when authorizing a dashboard request.
+            /// </summary>
+            public Func<HttpContext, bool> AuthorizationCallback { get; set; }
+        }
+    }
+}
---
+++ src/Modules/SimplCommerce.Module.HangfireJobs/Services/IBackgroundJobManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SimplCommerce.Module.HangfireJobs.Services
+{
+    /// <summary>
+    /// Defines interface of a job manager.
+    /// </summary>
+    public interface IBackgroundJobManager
+    {
+        /// <summary>
+        /// Enqueues a job to be executed.
+        /// </summary>
+        /// <typeparam name="TArgs">Type of the arguments of job.</typeparam>
+        /// <param name="args">Job arguments.</param>
+        /// <param name="delay">Job delay (wait duration before first try).</param>
+        /// <returns>Unique identifier of a background job.</returns>
+        Task<string> EnqueueAsync<TArgs>(TArgs args, TimeSpan? delay = null);
+    }
+}
