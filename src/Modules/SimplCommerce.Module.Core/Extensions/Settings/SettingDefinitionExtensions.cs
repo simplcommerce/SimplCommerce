bdb220b115eead5700b3cb42c67d7924a820a9dd
农志升 <nongzhsh@outlook.com>
2018-12-11 22:30:56 +0800

    add summary

--- src/Modules/SimplCommerce.Module.Core/Extensions/Settings/SettingDefinitionExtensions.cs
+++ src/Modules/SimplCommerce.Module.Core/Extensions/Settings/SettingDefinitionExtensions.cs
@@ -5,6 +5,11 @@ namespace SimplCommerce.Module.Core
 {
     public static class SettingDefinitionExtensions
     {
+        /// <summary>
+        /// Can use this method add default user settings in other module.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="defaultSettings"></param>
         public static void AddSettingDefinitionItems(this IApplicationBuilder app, params SettingDefinition[] defaultSettings)
         {
             var settingDefinitionProvider = app.ApplicationServices.GetService<SettingDefinitionProvider>();
