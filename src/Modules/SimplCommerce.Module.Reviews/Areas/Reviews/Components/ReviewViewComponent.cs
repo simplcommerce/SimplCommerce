035a1d538102a78a9bf7f417345fba300896e073
ADRIAN\adrianBasa <adryangolubey@yahoo.com>
2018-12-24 19:16:17 +0200

    remove  extra space

--- src/Modules/SimplCommerce.Module.Reviews/Areas/Reviews/Components/ReviewViewComponent.cs
+++ src/Modules/SimplCommerce.Module.Reviews/Areas/Reviews/Components/ReviewViewComponent.cs
@@ -12,19 +12,15 @@ using SimplCommerce.Module.Reviews.Models;
 namespace SimplCommerce.Module.Reviews.Areas.Reviews.Components
 {
     public class ReviewViewComponent : ViewComponent
-    {  
-        private readonly IRepository<Review> _reviewRepository;
+    {   private readonly IRepository<Review> _reviewRepository;
         private readonly IWorkContext _workContext;
         private readonly IRepository<Order> _orderRepository;
-       
-
         public ReviewViewComponent(IRepository<Review> reviewRepository, IWorkContext workContext, IRepository<Order> orderRepository )
         {
             _reviewRepository = reviewRepository;
             _workContext = workContext;
             _orderRepository = orderRepository;
         }
-
         public async Task<IViewComponentResult> InvokeAsync(long entityId, string entityTypeId)
         {
             var model = new ReviewVm
@@ -32,7 +28,6 @@ namespace SimplCommerce.Module.Reviews.Areas.Reviews.Components
                 EntityId = entityId,
                 EntityTypeId = entityTypeId
             };
-
             model.Items.Data = await _reviewRepository
                 .Query()
                 .Where(x => (x.EntityId == entityId) && (x.EntityTypeId == entityTypeId) && (x.Status == ReviewStatus.Approved))
@@ -56,35 +51,27 @@ namespace SimplCommerce.Module.Reviews.Areas.Reviews.Components
                         })
                         .ToList()
                 }).ToListAsync();
-
             model.ReviewsCount = model.Items.Data.Count;
             model.Rating1Count = model.Items.Data.Count(x => x.Rating == 1);
             model.Rating2Count = model.Items.Data.Count(x => x.Rating == 2);
             model.Rating3Count = model.Items.Data.Count(x => x.Rating == 3);
             model.Rating4Count = model.Items.Data.Count(x => x.Rating == 4);
             model.Rating5Count = model.Items.Data.Count(x => x.Rating == 5);
-
             if (User.Identity.IsAuthenticated == true)
             {
                 var user = await _workContext.GetCurrentUser();
                 model.LoggedUserName = user.FullName;
-
                 var currentUserOrder = _orderRepository
                     .Query()
                     .Where(x => x.CreatedById == user.Id);
                 var userOrderedProduct = currentUserOrder
                     .Where(x => x.OrderItems.Where(y =>y.ProductId == entityId).Count() > 0);
-
                  model.HasBoughtProduct = userOrderedProduct.Any() ? true : false;
-
              }
             else
-            {
-                model.LoggedUserName = string.Empty;
-                model.HasBoughtProduct = false;
-               
+            {   model.LoggedUserName = string.Empty;
+                model.HasBoughtProduct = false;     
             }
-
             return View(this.GetViewPath(), model);
         }
     }
