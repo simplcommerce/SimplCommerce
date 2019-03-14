10d6c5e33f18b767e4c0159fc011cc8d0e286ade
Thien Nguyen <nlqthien@gmail.com>
2019-03-14 14:40:54 +0700

    complete

--- src/Modules/SimplCommerce.Module.PaymentNganLuong/Data/PaymentNganLuongCustomerModelBuilder.cs
+++ src/Modules/SimplCommerce.Module.PaymentNganLuong/Data/PaymentNganLuongCustomerModelBuilder.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SimplCommerce.Infrastructure.Data;
 using SimplCommerce.Module.Payments.Models;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace SimplCommerce.Module.PaymentNganLuong.Data
 {
@@ -12,7 +9,7 @@ namespace SimplCommerce.Module.PaymentNganLuong.Data
         public void Build(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<PaymentProvider>().HasData(
-                new PaymentProvider("NganLuong") { Name = "Ngan Luong Payment", LandingViewComponentName = "NganLuongLanding", ConfigureUrl = "payments-nganluong-config", IsEnabled = true, AdditionalSettings = "{\"IsSandbox\":true,\"PartnerCode\":\"MOMOIQA420180417\",\"AccessKey\":\"SvDmj2cOTYZmQQ3H\",\"SecretKey\":\"PPuDXq1KowPT1ftR8DvlQTHhC03aul17\",\"PaymentFee\":0.0}" }
+                new PaymentProvider("NganLuong") { Name = "Ngan Luong Payment", LandingViewComponentName = "NganLuongLanding", ConfigureUrl = "payments-nganluong-config", IsEnabled = true, AdditionalSettings = "{\"IsSandbox\":true, \"MerchantId\": 47249, \"MerchantPassword\": \"e530745693dbde678f9da98a7c821a07\", \"ReceiverEmail\": \"nlqthien@gmail.com\"}" }
             );
         }
     }
--- src/SimplCommerce.WebHost/Migrations/20190314044223_AddedNganLuongPayment.Designer.cs
+++ src/SimplCommerce.WebHost/Migrations/20190314044223_AddedNganLuongPayment.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SimplCommerce.Module.Core.Data;
 
 namespace SimplCommerce.WebHost.Migrations
 {
     [DbContext(typeof(SimplDbContext))]
-    partial class SimplDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190314044223_AddedNganLuongPayment")]
+    partial class AddedNganLuongPayment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
