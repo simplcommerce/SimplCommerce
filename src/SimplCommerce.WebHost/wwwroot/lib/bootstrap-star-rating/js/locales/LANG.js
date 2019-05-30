32e38935a5f9ce1fedf142fbea0e26a40a5c50dd
Anderson Fernandes do Nascimento <andersonafn@gmail.com>
2019-05-30 13:59:46 -0300

    Localization adjustments

---
+++ src/SimplCommerce.WebHost/wwwroot/lib/bootstrap-star-rating/js/locales/LANG.js
@@ -0,0 +1,31 @@
+/*!
+ * Star Rating <LANG> Translations
+ *
+ * This file must be loaded after 'star-rating.js'. Patterns in braces '{}', or
+ * any HTML markup tags in the messages must not be converted or translated.
+ *
+ * NOTE: this file must be saved in UTF-8 encoding.
+ *
+ * @see http://github.com/kartik-v/bootstrap-star-rating
+ * @author Kartik Visweswaran <kartikv2@gmail.com>
+ */
+(function ($) {
+    "use strict";
+    $.fn.ratingLocales['<LANG>'] = {
+        defaultCaption: '{rating} Stars',
+        starCaptions: {
+            0.5: 'Half Star',
+            1: 'One Star',
+            1.5: 'One & Half Star',
+            2: 'Two Stars',
+            2.5: 'Two & Half Stars',
+            3: 'Three Stars',
+            3.5: 'Three & Half Stars',
+            4: 'Four Stars',
+            4.5: 'Four & Half Stars',
+            5: 'Five Stars'
+        },
+        clearButtonTitle: 'Clear',
+        clearCaption: 'Not Rated'
+    };
+})(window.jQuery);
