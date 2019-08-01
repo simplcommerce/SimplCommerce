023daa86c921e0cee774e754991f893b78947b3f
Rajganesh Mountbatton <rajganesh.mountbatton@honeywell.com>
2019-08-01 14:53:36 +0530

    Cashfree payment module created. This will be supported for India.

---
+++ src/Modules/SimplCommerce.Module.PaymentCashfree/wwwroot/admin/cashfree/cashfree-config-form.js
@@ -0,0 +1,40 @@
+﻿/*global angular, jQuery*/
+(function ($) {
+    angular
+        .module('simplAdmin.paymentCashfree')
+        .controller('CashfreeConfigFormCtrl', CashfreeConfigFormCtrl);
+
+    /* @ngInject */
+    function CashfreeConfigFormCtrl($state, paymentCashfreeService, translateService) {
+        var vm = this;
+        vm.translate = translateService;
+        vm.cashfreeConfig = {};
+
+        vm.save = function save() {
+            vm.validationErrors = [];
+            paymentCashfreeService.updateSetting(vm.cashfreeConfig)
+                .then(function (result) {
+                    toastr.success('Application settings have been saved');
+                })
+                .catch(function (response) {
+                    var error = response.data;
+                    vm.validationErrors = [];
+                    if (error && angular.isObject(error)) {
+                        for (var key in error) {
+                            vm.validationErrors.push(error[key][0]);
+                        }
+                    } else {
+                        vm.validationErrors.push('Could not save Cashfree settings.');
+                    }
+                });
+        };
+
+        function init() {
+            paymentCashfreeService.getSettings().then(function (result) {
+                vm.cashfreeConfig = result.data;
+            });
+        }
+
+        init();
+    }
+})(jQuery);
