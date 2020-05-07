045fcf086fd56c921c4014bf4d33df29100e150a
Florian Hebel <fhebel@users.noreply.github.com>
2020-05-07 13:49:38 +0200

    Added UI for editing ContactArea translations in the AdminUI (#890)

---
+++ src/Modules/SimplCommerce.Module.Contacts/Areas/Contacts/ViewModels/ContactAreaTranslationForm.cs
@@ -0,0 +1,9 @@
+﻿namespace SimplCommerce.Module.Contacts.Areas.Contacts.ViewModels
+{
+    public class ContactAreaTranslationForm
+    {
+        public string DefaultCultureName { get; set; }
+
+        public string Name { get; set; }
+    }
+}
