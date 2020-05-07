2c080612555884d7bf9c74fc039ba371eca36fd1
Florian Hebel <fhebel@users.noreply.github.com>
2020-05-07 14:14:44 +0200

    Added setting for DefaultCultureAdminUI (#890) … In my particular case I needed the shop to be in German (Global.DefaultCultureUI=de-DE), but I wanted to administer the products and everything else in English (Global.DefaultCultureAdminUI=en-GB, providing German translations), in order to offer additional languages (in the future) with fallback to English instead of German. ef migration missing because another name for the setting might be chosen

--- src/Modules/SimplCommerce.Module.Core/Data/CoreSeedData.cs
+++ src/Modules/SimplCommerce.Module.Core/Data/CoreSeedData.cs
@@ -13,6 +13,7 @@ namespace SimplCommerce.Module.Core.Data
                 new AppSetting("Global.AssetBundling") { Module = "Core", IsVisibleInCommonSettingPage = true, Value = "false" },
                 new AppSetting("Theme") { Module = "Core", IsVisibleInCommonSettingPage = false, Value = "Generic" },
                 new AppSetting("Global.DefaultCultureUI") { Module = "Core", IsVisibleInCommonSettingPage = true, Value = "en-US" },
+                new AppSetting("Global.DefaultCultureAdminUI") { Module = "Core", IsVisibleInCommonSettingPage = true, Value = "en-US" },
                 new AppSetting("Global.CurrencyCulture") { Module = "Core", IsVisibleInCommonSettingPage = true, Value = "en-US" },
                 new AppSetting("Global.CurrencyDecimalPlace") { Module = "Core", IsVisibleInCommonSettingPage = true, Value = "2" }
             );
