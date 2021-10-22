415394553cc913804e0cc2ac53f3f5fe00c9f7d2
Thien Nguyen <thienn@outlook.com>
2021-10-23 00:44:02 +0700

    remove startup.cs

--- src/SimplCommerce.WebHost/Program.cs
+++ src/SimplCommerce.WebHost/Program.cs
@@ -1,43 +1,118 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using MediatR;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Serilog;
+using Microsoft.Extensions.WebEncoders;
+using Microsoft.OpenApi.Models;
+using SimplCommerce.Infrastructure;
+using SimplCommerce.Infrastructure.Data;
+using SimplCommerce.Infrastructure.Modules;
+using SimplCommerce.Infrastructure.Web;
+using SimplCommerce.Module.Core.Data;
 using SimplCommerce.Module.Core.Extensions;
+using SimplCommerce.Module.Localization.Extensions;
+using SimplCommerce.Module.Localization.TagHelpers;
+using SimplCommerce.WebHost.Extensions;
 
-namespace SimplCommerce.WebHost
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Configuration.AddEntityFrameworkConfig(options =>
+    options.UseSqlServer(connectionString));
+
+GlobalConfiguration.WebRootPath = builder.Environment.WebRootPath;
+GlobalConfiguration.ContentRootPath = builder.Environment.ContentRootPath;
+
+builder.Services.AddModules();
+builder.Services.AddCustomizedDataStore(builder.Configuration);
+builder.Services.AddCustomizedIdentity(builder.Configuration);
+builder.Services.AddHttpClient();
+builder.Services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddTransient(typeof(IRepositoryWithTypedId<,>), typeof(RepositoryWithTypedId<,>));
+builder.Services.AddScoped<SlugRouteValueTransformer>();
+
+builder.Services.AddCustomizedLocalization();
+
+builder.Services.AddCustomizedMvc(GlobalConfiguration.Modules);
+builder.Services.Configure<RazorViewEngineOptions>(
+    options => { options.ViewLocationExpanders.Add(new ThemeableViewLocationExpander()); });
+builder.Services.Configure<WebEncoderOptions>(options =>
 {
-    public static class Program
+    options.TextEncoderSettings = new TextEncoderSettings(UnicodeRanges.All);
+});
+builder.Services.AddScoped<ITagHelperComponent, LanguageDirectionTagHelperComponent>();
+builder.Services.AddTransient<IRazorViewRenderer, RazorViewRenderer>();
+builder.Services.AddAntiforgery(options => options.HeaderName = "X-XSRF-Token");
+builder.Services.AddCloudscribePagination();
+
+foreach (var module in GlobalConfiguration.Modules)
+{
+    var moduleInitializerType = module.Assembly.GetTypes()
+       .FirstOrDefault(t => typeof(IModuleInitializer).IsAssignableFrom(t));
+    if ((moduleInitializerType != null) && (moduleInitializerType != typeof(IModuleInitializer)))
     {
-        public static void Main(string[] args)
-        {
-            BuildWebHost2(args).Build().Run();
-        }
-
-        // Changed to BuildWebHost2 to make EF don't pickup during design time
-        private static IHostBuilder BuildWebHost2(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => {
-                    webBuilder.UseStartup<Startup>();
-                    webBuilder.ConfigureAppConfiguration(SetupConfiguration);
-                    webBuilder.ConfigureLogging(SetupLogging);
-                 });
-
-        private static void SetupConfiguration(WebHostBuilderContext hostingContext, IConfigurationBuilder configBuilder)
-        {
-            var env = hostingContext.HostingEnvironment;
-            var configuration = configBuilder.Build();
-            configBuilder.AddEntityFrameworkConfig(options =>
-                    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"))
-            );
-            Log.Logger = new LoggerConfiguration()
-                       .ReadFrom.Configuration(configuration)
-                       .CreateLogger();
-        }
-        private static void SetupLogging(WebHostBuilderContext hostingContext, ILoggingBuilder loggingBuilder)
-        {
-            loggingBuilder.AddSerilog();
-        }
+        var moduleInitializer = (IModuleInitializer)Activator.CreateInstance(moduleInitializerType);
+        builder.Services.AddSingleton(typeof(IModuleInitializer), moduleInitializer);
+        moduleInitializer.ConfigureServices(builder.Services);
     }
 }
+
+builder.Services.AddScoped<ServiceFactory>(p => p.GetService);
+builder.Services.AddScoped<IMediator, Mediator>();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "SimplCommerce API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseWhen(
+        context => !context.Request.Path.StartsWithSegments("/api", StringComparison.OrdinalIgnoreCase),
+        a => a.UseExceptionHandler("/Home/Error")
+    );
+    app.UseHsts();
+}
+
+app.UseWhen(
+    context => !context.Request.Path.StartsWithSegments("/api", StringComparison.OrdinalIgnoreCase),
+    a => a.UseStatusCodePagesWithReExecute("/Home/ErrorWithCode/{0}")
+);
+
+app.UseHttpsRedirection();
+app.UseCustomizedStaticFiles(builder.Environment);
+app.UseRouting();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "SimplCommerce API V1");
+});
+app.UseCookiePolicy();
+app.UseCustomizedIdentity();
+app.UseCustomizedRequestLocalization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDynamicControllerRoute<SlugRouteValueTransformer>("/{**slug}");
+    endpoints.MapControllerRoute(
+        name: "areas",
+        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+var moduleInitializers = app.Services.GetServices<IModuleInitializer>();
+foreach (var moduleInitializer in moduleInitializers)
+{
+    moduleInitializer.Configure(app, builder.Environment);
+}
+
+app.Run();
