b3094cf1e1ecd6384b3184dbd2f424bb31e8308f
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2024-01-05 18:10:54 +0300

    Update net7.0 packages

--- src/SimplCommerce.WebHost/Migrations/20230930154048_InitDb.Designer.cs
+++ src/SimplCommerce.WebHost/Migrations/20230930154048_InitDb.Designer.cs
@@ -20,7 +20,7 @@ namespace SimplCommerce.WebHost.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.10")
+                .HasAnnotation("ProductVersion", "8.0.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
