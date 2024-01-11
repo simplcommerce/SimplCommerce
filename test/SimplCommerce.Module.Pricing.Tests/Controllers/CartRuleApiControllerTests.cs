e8d35b99aa7aa4415f22e2c37c736a5a83fc2954
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2024-01-11 09:07:11 +0300

    Update test/SimplCommerce.Module.Pricing.Tests/Controllers/CartRuleApiControllerTests.cs

--- test/SimplCommerce.Module.Pricing.Tests/Controllers/CartRuleApiControllerTests.cs
+++ test/SimplCommerce.Module.Pricing.Tests/Controllers/CartRuleApiControllerTests.cs
@@ -34,6 +34,7 @@ namespace SimplCommerce.Module.Pricing.Tests.Controllers
                 Sort = new Sort() { }
             };
             
+
             // Act
             var result = controller.List(smartTableParam);
             
