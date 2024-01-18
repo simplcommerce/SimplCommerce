7fb6d40774ca90fde707453b5878c5838f9bc4ca
Thien Nguyen <thienn@outlook.com>
2024-01-18 13:55:42 +0700

    temporary comment out setting decimal for ef migration to work

--- src/Modules/SimplCommerce.Module.Core/Data/SimplDbContext.cs
+++ src/Modules/SimplCommerce.Module.Core/Data/SimplDbContext.cs
@@ -49,12 +49,13 @@ namespace SimplCommerce.Module.Core.Data
 
             RegisterCustomMappings(modelBuilder, typeToRegisters);
 
-            foreach (var property in modelBuilder.Model.GetEntityTypes()
-                .SelectMany(t => t.GetProperties())
-                .Where(p => p.ClrType == typeof(decimal) || p.ClrType == typeof(decimal?)))
-            {
-                property.SetColumnType("decimal(18,2)");
-            }
+            //foreach (var property in modelBuilder.Model.GetEntityTypes()
+            //    .SelectMany(t => t.GetProperties())
+            //    .Where(p => p.ClrType == typeof(decimal) || p.ClrType == typeof(decimal?)))
+            //{
+            //    Console.WriteLine("decimal property: " + property.Name);
+            //    //property.SetColumnType("decimal(18,2)");
+            //}
 
             if (Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
             {
