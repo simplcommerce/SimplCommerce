fc36452f369d253cb875fcae08bd11f1583695ff
e <ermir.pellumbi@advania.is>
2025-01-05 20:54:30 +0000

    adding dorectory package props

--- src/Modules/SimplCommerce.Module.PaymentStripe/Areas/PaymentStripe/Controllers/StripeController.cs
+++ src/Modules/SimplCommerce.Module.PaymentStripe/Areas/PaymentStripe/Controllers/StripeController.cs
@@ -29,6 +29,7 @@ namespace SimplCommerce.Module.PaymentStripe.Areas.PaymentStripe.Controllers
         private readonly IRepositoryWithTypedId<PaymentProvider, string> _paymentProviderRepository;
         private readonly IRepository<Payment> _paymentRepository;
         private readonly ICurrencyService _currencyService;
+        private readonly IStripeClient _stripeClient;
 
         public StripeController(
             ICheckoutService checkoutService,
@@ -36,7 +37,8 @@ namespace SimplCommerce.Module.PaymentStripe.Areas.PaymentStripe.Controllers
             IWorkContext workContext,
             IRepositoryWithTypedId<PaymentProvider, string> paymentProviderRepository,
             IRepository<Payment> paymentRepository,
-            ICurrencyService currencyService)
+            ICurrencyService currencyService,
+            IStripeClient stripeClient)
         {
             _checkoutService = checkoutService;
             _orderService = orderService;
@@ -44,71 +46,72 @@ namespace SimplCommerce.Module.PaymentStripe.Areas.PaymentStripe.Controllers
             _paymentProviderRepository = paymentProviderRepository;
             _paymentRepository = paymentRepository;
             _currencyService = currencyService;
+            _stripeClient = stripeClient;
         }
 
-        public async Task<IActionResult> Charge(string stripeEmail, string stripeToken, Guid checkoutId)
-        {
-            var stripeProvider = await _paymentProviderRepository.Query().FirstOrDefaultAsync(x => x.Id == PaymentProviderHelper.StripeProviderId);
-            var stripeSetting = JsonConvert.DeserializeObject<StripeConfigForm>(stripeProvider.AdditionalSettings);
-            var stripeChargeService = new ChargeService(stripeSetting.PrivateKey);
-            var currentUser = await _workContext.GetCurrentUser();
+		public async Task<IActionResult> Charge(string stripeEmail, string stripeToken, Guid checkoutId)
+		{
+			var stripeProvider = await _paymentProviderRepository.Query().FirstOrDefaultAsync(x => x.Id == PaymentProviderHelper.StripeProviderId);
+			var stripeSetting = JsonConvert.DeserializeObject<StripeConfigForm>(stripeProvider.AdditionalSettings);
+			var stripeChargeService = new ChargeService(_stripeClient); // TODO: Fix/Update this.
+			var currentUser = await _workContext.GetCurrentUser();
 
-            var cart = await _checkoutService.GetCheckoutDetails(checkoutId);
-            if (cart == null)
-            {
-                return NotFound();
-            }
+			var cart = await _checkoutService.GetCheckoutDetails(checkoutId);
+			if(cart == null)
+			{
+				return NotFound();
+			}
 
-            var orderCreationResult = await _orderService.CreateOrder(checkoutId, "Stripe", 0, OrderStatus.PendingPayment);
-            if(!orderCreationResult.Success)
-            {
-                TempData["Error"] = orderCreationResult.Error;
-                return Redirect("~/checkout/payment");
-            }
+			var orderCreationResult = await _orderService.CreateOrder(checkoutId, "Stripe", 0, OrderStatus.PendingPayment);
+			if(!orderCreationResult.Success)
+			{
+				TempData["Error"] = orderCreationResult.Error;
+				return Redirect("~/checkout/payment");
+			}
 
-            var order = orderCreationResult.Value;
-            var zeroDecimalOrderAmount = order.OrderTotal;
-            if(!CurrencyHelper.IsZeroDecimalCurrencies(_currencyService.CurrencyCulture))
-            {
-                zeroDecimalOrderAmount = zeroDecimalOrderAmount * 100;
-            }
+			var order = orderCreationResult.Value;
+			var zeroDecimalOrderAmount = order.OrderTotal;
+			if(!CurrencyHelper.IsZeroDecimalCurrencies(_currencyService.CurrencyCulture))
+			{
+				zeroDecimalOrderAmount = zeroDecimalOrderAmount * 100;
+			}
 
-            var regionInfo = new RegionInfo(_currencyService.CurrencyCulture.LCID);
-            var payment= new Payment()
-            {
-                OrderId = order.Id,
-                Amount = order.OrderTotal,
-                PaymentMethod = "Stripe",
-                CreatedOn = DateTimeOffset.UtcNow
-            };
-            try
-            {
-                var charge = stripeChargeService.Create(new ChargeCreateOptions
-                {
-                    Amount = (int)zeroDecimalOrderAmount,
-                    Description = "Sample Charge",
-                    Currency = regionInfo.ISOCurrencySymbol,
-                    SourceId = stripeToken
-                });
+			var regionInfo = new RegionInfo(_currencyService.CurrencyCulture.LCID);
+			var payment = new Payment()
+			{
+				OrderId = order.Id,
+				Amount = order.OrderTotal,
+				PaymentMethod = "Stripe",
+				CreatedOn = DateTimeOffset.UtcNow
+			};
+			try
+			{
+				var charge = stripeChargeService.Create(new ChargeCreateOptions
+				{
+					Amount = (int)zeroDecimalOrderAmount,
+					Description = "Sample Charge",
+					Currency = regionInfo.ISOCurrencySymbol,
+					Source = stripeToken
+				});
 
-                payment.GatewayTransactionId = charge.Id;
-                payment.Status = PaymentStatus.Succeeded;
-                order.OrderStatus = OrderStatus.PaymentReceived;
-                _paymentRepository.Add(payment);
-                await _paymentRepository.SaveChangesAsync();
-                return Redirect($"~/checkout/success?orderId={order.Id}");
-            }
-            catch(StripeException ex)
-            {
-                payment.Status = PaymentStatus.Failed;
-                payment.FailureMessage = ex.StripeError.Message;
-                order.OrderStatus = OrderStatus.PaymentFailed;
+				payment.GatewayTransactionId = charge.Id;
+				payment.Status = PaymentStatus.Succeeded;
+				order.OrderStatus = OrderStatus.PaymentReceived;
+				_paymentRepository.Add(payment);
+				await _paymentRepository.SaveChangesAsync();
+				return Redirect($"~/checkout/success?orderId={order.Id}");
+			}
+			catch(StripeException ex)
+			{
+				payment.Status = PaymentStatus.Failed;
+				payment.FailureMessage = ex.StripeError.Message;
+				order.OrderStatus = OrderStatus.PaymentFailed;
 
-                _paymentRepository.Add(payment);
-                await _paymentRepository.SaveChangesAsync();
-                TempData["Error"] = ex.StripeError.Message;
-                return Redirect($"~/checkout/error?orderId={order.Id}");
-            }
-        }
+				_paymentRepository.Add(payment);
+				await _paymentRepository.SaveChangesAsync();
+				TempData["Error"] = ex.StripeError.Message;
+				return Redirect($"~/checkout/error?orderId={order.Id}");
+			}
+		}
     }
 }
--- src/Modules/SimplCommerce.Module.PaymentStripe/Models/PaymentProviderHelper.cs
+++ src/Modules/SimplCommerce.Module.PaymentStripe/Models/PaymentProviderHelper.cs
@@ -1,7 +1,12 @@
-﻿namespace SimplCommerce.Module.PaymentStripe.Models
+﻿namespace SimplCommerce.Module.PaymentStripe.Models;
+
+public static class PaymentProviderHelper
 {
-    public static class PaymentProviderHelper
-    {
-        public static readonly string StripeProviderId = "Stripe";
-    }
+    public static readonly string StripeProviderId = "Stripe";
+}
+
+public class StripeSettings
+{
+    public string SecretKey { get; set; }
+    public string PublishableKey { get; set; }
 }
