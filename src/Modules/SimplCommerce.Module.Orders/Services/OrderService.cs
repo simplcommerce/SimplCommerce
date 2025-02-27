bc0df9cdc5d7cdd4bb4a4f9a564c9a1924c47d8f
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2025-02-27 04:47:18 +0300

    Apply suggestions from code review

--- src/Modules/SimplCommerce.Module.Orders/Services/OrderService.cs
+++ src/Modules/SimplCommerce.Module.Orders/Services/OrderService.cs
@@ -210,7 +210,7 @@ namespace SimplCommerce.Module.Orders.Services
                 PaymentMethod = paymentMethod,
                 PaymentFeeAmount = paymentFeeAmount
             };
-using (var transaction = _checkoutItemRepository.BeginTransaction()){
+        using (var transaction = _checkoutItemRepository.BeginTransaction()){
             foreach (var checkoutItem in checkout.CheckoutItems)
             {
                 if (!checkoutItem.Product.IsAllowToOrder || !checkoutItem.Product.IsPublished || checkoutItem.Product.IsDeleted)
@@ -259,9 +259,10 @@ using (var transaction = _checkoutItemRepository.BeginTransaction()){
                     checkoutItem.Product.StockQuantity = checkoutItem.Product.StockQuantity - checkoutItem.Quantity;
                 }
             }
+
             _checkoutItemRepository.SaveChanges();
             transaction.Commit();
-}
+        }
 
             order.OrderStatus = orderStatus;
             order.OrderNote = checkout.OrderNote;
