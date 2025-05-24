5e0a545ff0e5e34c1b9113fddfb4b7c0f42382b8
DELL <DELL@DESKTOP-8IDJ6GC>
2025-05-24 14:14:03 +0530

    Resolve:Add Product Feed Generation Endpoint #1133

---
+++ src/Modules/SimplCommerce.Module.Catalog/Areas/Catalog/Controllers/FeedController.cs
@@ -0,0 +1,26 @@
+﻿using SimplCommerce.Module.Catalog.Models;
+using SimplCommerce.Module.Catalog.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using SimplCommerce.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace SimplCommerce.Module.Catalog.Controllers
+{
+    [Area("Catalog")]
+    public class FeedController : Controller
+    {
+        private readonly IRepository<Product> _productRepository;
+
+        public FeedController(IRepository<Product> productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _productRepository.Query().Include(p => p.ThumbnailImage).ToList();
+            return View(products);
+        }
+    }
+}
