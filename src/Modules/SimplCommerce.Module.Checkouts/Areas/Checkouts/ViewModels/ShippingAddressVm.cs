4dac3315782f5f6f85bd3a219b80ec64a4e433ba
Jayanth T <jayanthtrilok@outlook.com>
2026-03-08 21:23:57 +0530

    Code fix for Phone field accepting non numeric characters

--- src/Modules/SimplCommerce.Module.Checkouts/Areas/Checkouts/ViewModels/ShippingAddressVm.cs
+++ src/Modules/SimplCommerce.Module.Checkouts/Areas/Checkouts/ViewModels/ShippingAddressVm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,9 @@ namespace SimplCommerce.Module.Checkouts.Areas.Checkouts.ViewModels
 
         public string ContactName { get; set; }
 
+        [Required(ErrorMessage = "Phone number is required")]
+        [Phone(ErrorMessage = "Please enter a valid phone number")]
+        [StringLength(20, MinimumLength = 10, ErrorMessage = "Phone number must be between 10 and 20 characters")]
         public string Phone { get; set; }
 
         public string AddressLine1 { get; set; }
